b979daee4895bbe921f30514fb7572437a84fc88
omgzsa <zselloandras@gmail.com>
2023-10-02 16:03:53 +0200

    feat: category navbar/items started

--- components/category/navbar/index.vue
+++ components/category/navbar/index.vue
@@ -1,9 +1,41 @@
 <script setup>
-//
+const { getItems } = useDirectusItems();
+
+const { data } = await useAsyncData('alKategoriak', () => 
+  getItems({
+    collection: 'termekAlKategoria',
+    fields: ['id', 'termekAlKategoriaNev', 'termekAlKategoriaKep'],
+    params: {
+      filter: {  kapcsolodoKategoria: { _eq: "1" }  }
+    }
+  })
+);
+
+// console.log(alKategoriak.value);
 </script>
 
 <template>
-  <div>
-    <CategoryNavbarItem />
+  <div class="bg-gray-100 ">
+    <div class="flex flex-row py-10 gap-x-4 site-padding">
+      <div class="flex flex-row gap-x-4">
+        <div class="w-60 md:w-72">
+          <NuxtLink to="/kerekpartartok">
+            <NuxtImg 
+              src="/tetocsomagtartok-thumb-nav.webp"
+              alt="Összes kerékpártartó"
+              height="170px"
+              width="295px"
+            />
+            <p class="h-20 p-4 bg-dark-100 text-accent-100">Összes kerékpártartó</p>
+          </NuxtLink>
+        </div>
+        <CategoryNavbarItem
+          v-for="thumb in data" :key="thumb.id"
+          :title="thumb.termekAlKategoriaNev" 
+          :image="thumb.termekAlKategoriaKep" 
+          :to="`/kerekpartartok`"
+        />
+      </div>
+    </div>
   </div>
 </template>
--- components/category/navbar/item.vue
+++ components/category/navbar/item.vue
@@ -1,9 +1,42 @@
 <script setup>
+const config = useRuntimeConfig();
+const baseUrl = config.public.directusUrl;
 
+const { defaultTransition } = useTailwindConfig()
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  to: {
+    type: String,
+  },
+  isActive: {
+    type: Boolean
+  }
+})
 </script>
 
 <template>
-  <div>
-    navbar-item
+  <div class="bg-white w-60 md:w-72 text-dark-100 hover:text-accent-100 hover:bg-dark-100"
+    :class="defaultTransition"
+  >
+    <NuxtLink :to="`${to}`">
+      <NuxtImg 
+        :src="`${baseUrl}/assets/${image}`"
+        :alt="title"
+        height="170px"
+        width="295px"
+      />
+      <p class="h-20 p-4 ">
+        {{ title }}
+      </p>
+    </NuxtLink>
   </div>
 </template>
+
