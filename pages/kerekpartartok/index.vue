42e3e26bc7848c59c73571aa876dc4f40cbeb079
omgzsa <zselloandras@gmail.com>
2023-10-02 16:06:25 +0200

    docs: main category pages started

--- pages/kerekpartartok/index.vue
+++ pages/kerekpartartok/index.vue
@@ -1,48 +1,22 @@
 <script setup>
-const config = useRuntimeConfig();
-const baseUrl = config.public.directusUrl;
-
-const { getItems } = useDirectusItems();
-
-const { data: alKategoriak, pending, error } = await useAsyncData('alKategoriak', () => 
-  getItems({
-    collection: 'termekAlKategoria',
-    fields: ['id', 'termekAlKategoriaNev', 'termekAlKategoriaKep'],
-    // params: {
-    //   filter: {  id: { "_eq": "2" }  }
-    // }
-  })
-);
-
-// const { data: kategoriak, pending, error } = await useAsyncData('kategoriak', () =>
-//   getItems({
-//     collection: 'termekKategoria',
-//     fields: ['id', 'termekKategoriaNev'],
-//   })
-// );
+//
 </script>
 
 <template>
-  <div>
+  <div class="bg-white">
     <CategoryNavbar />
-    <AppHeader>
-      <template #title>
-        Kerékpártartók
-      </template>
-      <template #description>
-        Ezeknél a tartóknál mindenképpen ajánlott a Thule 976 világítás és rendszámtábla tartó használata, különben a kerékpártartó nem felel meg a jelenlegi KRESZ előírásoknak!
-      </template>
-    </AppHeader>
-    <div>
+    <div class="space-y-12 site-padding">
+      <AppHeader>
+        <template #title>
+          Kerékpártartók
+        </template>
+        <template #description>
+          Ezeknél a tartóknál mindenképpen ajánlott a Thule 976 világítás és rendszámtábla tartó használata, különben a kerékpártartó nem felel meg a jelenlegi KRESZ előírásoknak!
+        </template>
+      </AppHeader>
       <div>
-        <div v-if="pending" class="flex items-center justify-center h-12 text-xl font-bold text-white bg-red-500">LOADING ...</div>
+        PRODUCT ARCHIVE      
       </div>
-      <div>
-        <pre v-for="item in alKategoriak" :key="item.id">
-          {{ item }}
-        </pre>
-      </div>
-      
     </div>
       
   </div>
