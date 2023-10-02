c17e559b617e772e2edbae1d40fa47098117d31d
omgzsa <zselloandras@gmail.com>
2023-10-02 10:47:47 +0200

    feat: header component for product pages

---
+++ components/app/header.vue
@@ -0,0 +1,14 @@
+<script setup>
+//
+</script>
+
+<template>
+  <div class="max-w-2xl">
+    <h1>
+      <slot name="title" />
+    </h1>
+    <p>
+      <slot name="description" />
+    </p>
+  </div>
+</template>
