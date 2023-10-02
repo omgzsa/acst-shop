a12f318c168f08e92c9754d78740b97a3316f781
omgzsa <zselloandras@gmail.com>
2023-10-02 10:48:46 +0200

    feat: dynamic product page added

--- pages/kerekpartartok/[category]/[id].vue
+++ pages/kerekpartartok/[category]/[id].vue
@@ -1,31 +1,43 @@
 <script setup>
-const route = useRoute();
+const config = useRuntimeConfig();
+const baseUrl = config.public.directusUrl;
 
-// const { getItemById } = useDirectusItems();
-// const {
-//   data: termek,
-//   pending,
-//   error,
-//   refresh,
-// } = await useAsyncData("termek", () =>
-//   getItemById({
-//     collection: termek,
-//     id: route.params.id,
-//     params: route.params.category,
-//   })
-// );
+const { getItemById } = useDirectusItems();
+const { params, path } = useRoute()
 
-// console.log(termek)
+const product = ref({})
+
+const { data, pending, error } = await useAsyncData(path, () => {
+  return getItemById({
+    collection: "termekek",
+    id: params.id,
+  });
+},
+{
+   pick: ['termekNev', 'termekLeiras', 'termekKep'],
+})
+
+product.value = data.value
 </script>
 
 <template>
   <div>
-    <p>
-      Itt egy termék a <strong>{{ route.params.category }}</strong> kategóriából,
-    azon belül az <strong>{{ route.params.id }}</strong> termék.
+    <div v-if="pending" class="flex items-center justify-center h-12 text-xl font-bold text-white bg-red-500">LOADING ...</div>
+    <div v-if="error" class="h-12 mx-auto my-auto bg-red-400">{{ error.message }}</div>
+    <div v-else>
+      <p>
+      Itt egy termék a <strong>{{ params.category }}</strong> kategóriából,
+    azon belül az <strong>{{ params.id }}</strong> termék.
     </p>
-    <!-- <div class="max-w-xl">
-      <code>{{ termek }}</code>
-    </div> -->
+    <div>
+      <!-- <code>{{ product }}</code> -->
+      <h1>{{ product.termekNev }}</h1>
+      <p>{{ product.termekLeiras }}</p>
+      <NuxtImg 
+        :src="`${baseUrl}/assets/${product.termekKep}`" :alt="product.termekLeiras" 
+        class="w-96 h-96"
+      />
+    </div>
+    </div>
   </div>
 </template>
