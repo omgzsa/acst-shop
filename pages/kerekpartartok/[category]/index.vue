1f6c65d5d214127038ae15d3db893cfb0e75fbac
omgzsa <zselloandras@gmail.com>
2023-10-04 11:14:07 +0200

    [feat]: dynamic sub-category page added

--- pages/kerekpartartok/[category]/index.vue
+++ pages/kerekpartartok/[category]/index.vue
@@ -1,9 +1,22 @@
 <script setup>
-const route = useRoute();
+const { path, params } = useRoute();
+// console.log(path === `/kerekpartartok/${params.category}`);
 </script>
 
 <template>
-  <div>
-    {{ route.params.category }}
+  <div class="bg-white">
+    {{ params.category }}
+    <CategoryNavbar />
+    <div class="space-y-12 site-padding">
+      <AppHeader>
+        <template #title> Kerékpártartók </template>
+        <template #description>
+          Ezeknél a tartóknál mindenképpen ajánlott a Thule 976 világítás és
+          rendszámtábla tartó használata, különben a kerékpártartó nem felel meg
+          a jelenlegi KRESZ előírásoknak!
+        </template>
+      </AppHeader>
+      <div>PRODUCT ARCHIVE</div>
+    </div>
   </div>
 </template>
