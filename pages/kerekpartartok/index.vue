e8b98713a4883d3cd0ce0a4437f29cc92a3a6551
omgzsa <zselloandras@gmail.com>
2023-10-04 11:13:29 +0200

    [style]: goin onward with category archive

--- pages/kerekpartartok/index.vue
+++ pages/kerekpartartok/index.vue
@@ -1,23 +1,37 @@
 <script setup>
-//
+const { getItems } = useDirectusItems();
+
+const { data } = await useAsyncData('id', () =>
+  getItems({
+    collection: 'termekek',
+    fields: ['id', 'termekNev', 'termekAr'],
+    // params: {
+    //   filter: { kapcsolodoKategoria: { _eq: '1' } },
+    // },
+  })
+);
+
+console.log(data.value);
 </script>
 
 <template>
   <div class="bg-white">
     <CategoryNavbar />
-    <div class="space-y-12 site-padding">
+    <div class="pt-14 space-y-14 site-padding">
       <AppHeader>
-        <template #title>
-          Kerékpártartók
-        </template>
+        <template #title> Kerékpártartók </template>
         <template #description>
-          Ezeknél a tartóknál mindenképpen ajánlott a Thule 976 világítás és rendszámtábla tartó használata, különben a kerékpártartó nem felel meg a jelenlegi KRESZ előírásoknak!
+          Ezeknél a tartóknál mindenképpen ajánlott a Thule 976 világítás és
+          rendszámtábla tartó használata, különben a kerékpártartó nem felel meg
+          a jelenlegi KRESZ előírásoknak!
         </template>
       </AppHeader>
       <div>
-        PRODUCT ARCHIVE      
+        <!-- <div v-for="item in data" :key="item.id">
+          <h3>{{ item.termekNev }}</h3>
+          <p>{{ item.termekAr }}</p>
+        </div> -->
       </div>
     </div>
-      
   </div>
 </template>
