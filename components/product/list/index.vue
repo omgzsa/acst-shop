c86cd7108d59508b4308bd99a190fa46fabcd13c
omgzsa <zselloandras@gmail.com>
2023-10-07 21:45:13 +0200

    [feat]: added category/subCat slugs to link to single page

--- components/product/list/index.vue
+++ components/product/list/index.vue
@@ -13,18 +13,17 @@ defineProps({
 
 <template>
   <div
-    class="grid grid-cols-2 pb-14 gap-x-4 gap-y-8 sm:grid-cols-3 lg:grid-cols-4"
+    class="grid grid-cols-2 pb-14 gap-x-4 gap-y-8 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5"
   >
     <ProductListItem
       v-for="product in items"
       :key="product.id"
       :id="product.id"
       :name="product.termekNev"
-      :price="product.termekAr"
       :description="product.termekLeiras"
+      :img="`${baseUrl}/assets/${product.termekKep}`"
       :category-slug="product.kapcsolodoKategoria.slug"
       :sub-category-slug="product.kapcsolodoAlKategoria.slug"
-      :img="`${baseUrl}/assets/${product.termekKep}`"
     />
   </div>
 </template>
