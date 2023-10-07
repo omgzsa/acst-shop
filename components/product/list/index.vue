cc404cec4da27ed6c9c1bccb1dcd976aa14d4588
omgzsa <zselloandras@gmail.com>
2023-10-07 17:15:21 +0200

    [feat]: v1 product archive

--- components/product/list/index.vue
+++ components/product/list/index.vue
@@ -1,4 +1,8 @@
 <script setup>
+const config = useRuntimeConfig();
+
+const baseUrl = config.public.directusUrl;
+
 defineProps({
   items: {
     type: Array,
@@ -8,12 +12,19 @@ defineProps({
 </script>
 
 <template>
-  <div>
+  <div
+    class="grid grid-cols-2 pb-14 gap-x-4 gap-y-8 sm:grid-cols-3 lg:grid-cols-4"
+  >
     <ProductListItem
       v-for="product in items"
       :key="product.id"
+      :id="product.id"
       :name="product.termekNev"
       :price="product.termekAr"
+      :description="product.termekLeiras"
+      :category-slug="product.kapcsolodoKategoria.slug"
+      :sub-category-slug="product.kapcsolodoAlKategoria.slug"
+      :img="`${baseUrl}/assets/${product.termekKep}`"
     />
   </div>
 </template>
