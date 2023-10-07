67f620f98ed26759a49940cd48ebbef423d55d99
omgzsa <zselloandras@gmail.com>
2023-10-07 17:16:07 +0200

    [feat]: connected single page to api

--- components/product/list/item/index.vue
+++ components/product/list/item/index.vue
@@ -1,21 +1,49 @@
 <script setup>
 defineProps({
+  id: {
+    type: Number,
+    required: true,
+  },
   name: {
     type: String,
-    required: true,
+    // required: true,
   },
   price: {
     type: Number,
-    required: true,
+    // required: true,
+  },
+  img: {
+    type: String,
+    // required: true,
+  },
+  description: {
+    type: String,
+    // required: false,
+  },
+  categorySlug: {
+    type: String,
+  },
+  subCategorySlug: {
+    type: String,
+    // required: false,
   },
 });
 </script>
 
 <template>
-  <div>
-    <div>
-      <h3>{{ name }}</h3>
-      <p>{{ price }}</p>
+  <NuxtLink :to="`/${categorySlug}/${subCategorySlug}/${id}`">
+    <div class="space-y-4">
+      <NuxtImg
+        :src="img"
+        :alt="name"
+        width="179"
+        height="240"
+        class="object-contain object-bottom py-6 bg-dark-200"
+      />
+      <div class="px-1 space-y-2">
+        <h4>{{ name }}</h4>
+        <p class="text-sm text-dark-300">{{ description.slice(0, 40) }}...</p>
+      </div>
     </div>
-  </div>
+  </NuxtLink>
 </template>
