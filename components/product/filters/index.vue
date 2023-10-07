317392be82f0163d15daaceefb7e3c7fa1b2af2d
omgzsa <zselloandras@gmail.com>
2023-10-07 21:44:14 +0200

    [feat]: hooked up quantity from products arr

--- components/product/filters/index.vue
+++ components/product/filters/index.vue
@@ -10,22 +10,18 @@ defineProps({
 </script>
 
 <template>
-  <div class="">
-    <div class="grid grid-cols-6 py-3 border-t-2 border-b-2 border-dark-200">
-      <details class="col-span-4 sm:col-span-5">
-        <summary
-          class="flex items-center justify-between list-none cursor-pointer"
-        >
-          <div class="flex items-center hover:font-semibold">
-            <Icon name="mdi:filter-variant" size="22" class="mb-0.5 mr-2" />
-            <span>Szűrés</span>
-          </div>
-        </summary>
-        <p class="pt-4">FILTERS</p>
-      </details>
-      <span class="col-span-2 ml-auto sm:col-span-1"
-        >{{ quantity }} termék</span
+  <div class="grid grid-cols-6 py-3 border-t-2 border-b-2 border-dark-200">
+    <details class="col-span-4 sm:col-span-5">
+      <summary
+        class="flex items-center justify-between list-none cursor-pointer"
       >
-    </div>
+        <div class="flex items-center hover:font-semibold">
+          <Icon name="mdi:filter-variant" size="22" class="mb-0.5 mr-2" />
+          <span>Szűrés</span>
+        </div>
+      </summary>
+      <p class="pt-4">FILTERS</p>
+    </details>
+    <span class="col-span-2 ml-auto sm:col-span-1">{{ quantity }} termék</span>
   </div>
 </template>
