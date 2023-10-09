431859aa29ad5855e1881375cd14e92ae22d59d3
omgzsa <zselloandras@gmail.com>
2023-10-09 19:06:30 +0200

    added a logo component

---
+++ components/TheLogo.vue
@@ -0,0 +1,39 @@
+<script setup>
+const { defaultTransition } = useTailwindConfig();
+</script>
+
+<template>
+  <NuxtLink to="/" class="inline-flex items-center mr-auto group nav__logo">
+    <div
+      class="flex flex-row items-center justify-center space-x-1 text-3xl group-hover:font-bold"
+    >
+      <span
+        class="text-dark-300 group-hover:text-accent-100"
+        :class="defaultTransition"
+      >
+        <div class="block group-hover:hidden">
+          <Icon name="ph:activity-bold" />
+        </div>
+        <div class="hidden group-hover:block">
+          <Icon name="mdi:music-accidental-sharp" />
+        </div>
+      </span>
+
+      <span
+        class="logo text-dark-100 group-hover:text-dark-100 group-hover:underline group-hover:underline-offset-4 group-hover:decoration-accent-100"
+        :class="defaultTransition"
+      >
+        ACST
+      </span>
+    </div>
+  </NuxtLink>
+</template>
+
+<style>
+/* @supports (-webkit-text-stroke: 0.5px black) {
+  .logo {
+    -webkit-text-fill-color: #b6dd47;
+    -webkit-text-stroke: 0.5px black;
+  }
+} */
+</style>
