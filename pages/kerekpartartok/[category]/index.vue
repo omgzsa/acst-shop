c35eeea23cf6715f4a74b086f503ded43b65634b
omgzsa <zselloandras@gmail.com>
2023-10-09 12:58:02 +0200

    [feat]: fleshing out dynamic sub-category pages

--- pages/kerekpartartok/[category]/index.vue
+++ pages/kerekpartartok/[category]/index.vue
@@ -1,20 +1,61 @@
 <script setup>
 const { path, params } = useRoute();
+const { getItems } = useDirectusItems();
+
+const { data, pending, error } = await useAsyncData('termekek', () =>
+  getItems({
+    collection: 'termekek',
+    params: {
+      fields: [
+        'id',
+        'termekNev',
+        'termekKep',
+        'termekLeiras',
+        'kapcsolodoKategoria.slug',
+        'kapcsolodoAlKategoria.termekAlKategoriaNev',
+        'kapcsolodoAlKategoria.slug',
+        'kapcsolodoAlKategoria.termekAlKategoriaLeiras',
+      ],
+      filter: {
+        kapcsolodoAlKategoria: {
+          slug: { _eq: params.category },
+        },
+      },
+    },
+  })
+);
+
+const pageTitle = computed(() => {
+  return data.value[0].kapcsolodoAlKategoria.termekAlKategoriaNev;
+});
+
+const pageDescription = computed(() => {
+  return data.value[0].kapcsolodoAlKategoria.termekAlKategoriaLeiras;
+});
+
+const productQuantity = computed(() => {
+  return data.value.length;
+});
+
+// console.log(data.value);
 </script>
 
 <template>
   <div class="bg-white">
     <CategoryNavbar />
-    <div class="space-y-12 site-padding">
+    <div class="pt-14 space-y-14 site-padding">
       <AppHeader>
-        <template #title> Kerékpártartók </template>
+        <template #title> {{ pageTitle }} </template>
         <template #description>
-          Ezeknél a tartóknál mindenképpen ajánlott a Thule 976 világítás és
-          rendszámtábla tartó használata, különben a kerékpártartó nem felel meg
-          a jelenlegi KRESZ előírásoknak!
+          {{ pageDescription }}
         </template>
       </AppHeader>
-      <div>PRODUCT ARCHIVE</div>
+
+      <!-- product filters section -->
+      <ProductFilters :quantity="productQuantity" />
+
+      <!-- product archive -->
+      <ProductList :items="data" />
     </div>
   </div>
 </template>
