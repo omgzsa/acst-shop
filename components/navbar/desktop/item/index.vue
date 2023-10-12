a1cde9f7412269842d728d6774ba97ce1c068eb7
omgzsa <zselloandras@gmail.com>
2023-10-12 11:41:46 +0200

    navbar-desktop-item v1

---
+++ components/navbar/desktop/item/index.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+  item: Object,
+  isVisible: Boolean,
+});
+</script>
+
+<template>
+  <li class="cursor-pointer group">
+    <span> {{ props.item.name }} </span>
+    <div class="absolute w-full">&nbsp;</div>
+    <Transition name="fade">
+      <div
+        v-show="isVisible"
+        class="absolute left-0 top-8 hidden w-[800px] px-6 py-4 overflow-hidden bg-white border rounded-lg shadow-sm menu-sub group-hover:block"
+      >
+        <ul class="grid grid-cols-4 gap-x-6 gap-y-8 space-y-0.5">
+          <NavbarDesktopItemSub
+            v-for="cat in props.item.categories"
+            :key="cat.id"
+            :name="cat.name"
+            :link="cat.link"
+            :sub-categories="cat.subCategories"
+          />
+        </ul>
+      </div>
+    </Transition>
+  </li>
+</template>
+
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
