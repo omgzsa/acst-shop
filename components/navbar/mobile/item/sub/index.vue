c386f0aa773db9400bd3529a4a2bc89b82722695
omgzsa <zselloandras@gmail.com>
2023-10-12 11:40:19 +0200

    navbar-mobile-item-sub

---
+++ components/navbar/mobile/item/sub/index.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+  name: String,
+  link: String,
+  subCategories: Array,
+});
+
+console.log(props.subCategories);
+</script>
+
+<template>
+  <div>
+    <div class="grid gap-y-2">
+      <span
+        ><NuxtLink
+          :to="`${props.link}`"
+          class="px-2 py-1 text-base font-semibold leading-snug tracking-wide transition-colors duration-300 rounded xl:text-lg hover:text-dark-100 hover:bg-accent-100 hover:font-bold"
+          >{{ props.name }}</NuxtLink
+        ></span
+      >
+      <ul class="grid gap-y-2">
+        <li
+          v-for="sub in props.subCategories"
+          :key="sub.id"
+          class="text-sm rounded-md hover:font-bold"
+        >
+          <NuxtLink
+            :to="`${sub.link}`"
+            class="px-2 py-1 transition-colors duration-300 rounded hover:text-dark-100 hover:bg-accent-100 hover:font-semibold"
+          >
+            {{ sub.name }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
