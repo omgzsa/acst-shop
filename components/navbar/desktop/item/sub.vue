2d6a486d666f7a0484024fd180ecc11e29087203
omgzsa <zselloandras@gmail.com>
2023-10-12 11:41:29 +0200

    navbar-desktop-item-sub v1

---
+++ components/navbar/desktop/item/sub.vue
@@ -0,0 +1,28 @@
+<script setup>
+const { defaultTransition } = useTailwindConfig();
+
+const props = defineProps({
+  name: String,
+  link: String,
+  subCategories: Array,
+});
+</script>
+
+<template>
+  <div>
+    <li
+      class="px-2 py-1 text-sm rounded-md hover:bg-accent-100 hover:font-bold"
+      :class="defaultTransition"
+    >
+      <NuxtLink class="w-full font-semibold" :to="link">{{ name }}</NuxtLink>
+    </li>
+    <li
+      v-for="sub in props.subCategories"
+      :key="sub.id"
+      class="px-2 py-1 text-sm rounded-md hover:bg-accent-100 hover:font-bold"
+      :class="defaultTransition"
+    >
+      <NuxtLink class="w-full" :to="sub.link">{{ sub.name }}</NuxtLink>
+    </li>
+  </div>
+</template>
