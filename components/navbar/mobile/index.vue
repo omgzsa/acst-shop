f1e583a8697b5589aec9aeb44317e29455243e65
omgzsa <zselloandras@gmail.com>
2023-10-12 11:41:06 +0200

    navbar-mobile v1 - wrapper

---
+++ components/navbar/mobile/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+const props = defineProps({
+  navItems: {
+    type: Array,
+    required: true,
+  },
+});
+
+// console.log(props.navItems);
+</script>
+
+<template>
+  <div class="grid gap-y-4">
+    <NavbarMobileItem
+      v-for="category in navItems"
+      :key="category.id"
+      :name="category.name"
+      :categories="category.categories"
+    />
+  </div>
+</template>
