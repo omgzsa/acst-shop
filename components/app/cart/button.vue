700ccea1e9a854a671293eabeae742c00ce7ff37
omgzsa <zselloandras@gmail.com>
2023-10-12 11:42:27 +0200

    [feat]: user / cart button (not functioning)

---
+++ components/app/cart/button.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { defaultTransition } = useTailwindConfig();
+</script>
+
+<template>
+  <div>
+    <Icon
+      name="mdi:cart-outline"
+      class="p-1.5 rounded-full w-9 h-9 bg-accent-100 hover:bg-dark-100 hover:text-dark-200 cursor-pointer"
+      :class="defaultTransition"
+    />
+  </div>
+</template>
