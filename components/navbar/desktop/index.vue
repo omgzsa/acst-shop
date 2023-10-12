d9a0e7a57acefe7d0fc43d651812f248291c81ae
omgzsa <zselloandras@gmail.com>
2023-10-12 11:42:02 +0200

    navbar-desktop v1 - wrapper

---
+++ components/navbar/desktop/index.vue
@@ -0,0 +1,28 @@
+<script setup>
+const props = defineProps({
+  navItems: Array,
+});
+
+const isVisible = ref(false);
+
+const menuHover = () => {
+  isVisible.value = true;
+};
+
+const menuLeave = () => {
+  isVisible.value = false;
+};
+</script>
+
+<template>
+  <ul class="relative hidden space-x-6 font-medium menu-main lg:flex">
+    <NavbarDesktopItem
+      v-for="category in navItems"
+      :key="category.id"
+      :item="category"
+      :is-visible="isVisible"
+      @mouseover="menuHover"
+      @mouseleave="menuLeave"
+    />
+  </ul>
+</template>
