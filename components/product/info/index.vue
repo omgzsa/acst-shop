4053b563c98aba997f55134b0babedee4879aa78
omgzsa <zselloandras@gmail.com>
2023-10-13 10:40:56 +0200

    starting single product page

---
+++ components/product/info/index.vue
@@ -0,0 +1,34 @@
+<script setup>
+const config = useRuntimeConfig();
+const baseUrl = config.public.directusUrl;
+
+const props = defineProps({
+  img: String,
+  gallery: Array,
+  name: String,
+  description: String,
+  price: Number,
+});
+</script>
+
+<template>
+  <div class="space-y-4">
+    <NuxtImg
+      :src="props.img"
+      :alt="props.description"
+      class="object-contain -mb-4 h-96 bg-dark-200"
+    />
+    <div class="grid grid-cols-5 gap-x-1">
+      <NuxtImg
+        v-for="img in gallery"
+        :key="img.id"
+        :src="`${baseUrl}/assets/${img.directus_files_id}`"
+      />
+    </div>
+    <div class="site-padding">
+      <h1>{{ props.name }}</h1>
+      <p>{{ props.description }}</p>
+      <p>{{ props.price }}</p>
+    </div>
+  </div>
+</template>
--- pages/kerekpartartok/[category]/[id].vue
+++ pages/kerekpartartok/[category]/[id].vue
@@ -3,41 +3,52 @@ const config = useRuntimeConfig();
 const baseUrl = config.public.directusUrl;
 
 const { getItemById } = useDirectusItems();
-const { params, path } = useRoute()
+const { params, path } = useRoute();
 
-const product = ref({})
+const product = ref({});
 
 const { data, pending, error } = await useAsyncData(path, () => {
   return getItemById({
-    collection: "termekek",
+    collection: 'termekek',
     id: params.id,
+    params: {
+      fields: [
+        'id',
+        'termekNev',
+        'termekKep',
+        'termekLeiras',
+        'termekAr',
+        'termekGaleria.id',
+        'termekGaleria.directus_files_id',
+      ],
+      // filter: { kapcsolodoKategoria: { id: { _eq: '1' } } },
+    },
   });
-},
-{
-   pick: ['termekNev', 'termekLeiras', 'termekKep'],
-})
+});
 
-product.value = data.value
+console.log(data.value);
+product.value = data.value;
 </script>
 
 <template>
-  <div>
-    <div v-if="pending" class="flex items-center justify-center h-12 text-xl font-bold text-white bg-red-500">LOADING ...</div>
-    <div v-if="error" class="h-12 mx-auto my-auto bg-red-400">{{ error.message }}</div>
-    <div v-else>
-      <p>
-      Itt egy termék a <strong>{{ params.category }}</strong> kategóriából,
-    azon belül az <strong>{{ params.id }}</strong> termék.
-    </p>
-    <div>
-      <!-- <code>{{ product }}</code> -->
-      <h1>{{ product.termekNev }}</h1>
-      <p>{{ product.termekLeiras }}</p>
-      <NuxtImg 
-        :src="`${baseUrl}/assets/${product.termekKep}`" :alt="product.termekLeiras" 
-        class="w-96 h-96"
-      />
+  <div class="bg-white">
+    <div
+      v-if="pending"
+      class="flex items-center justify-center h-12 text-xl font-bold text-white bg-red-500"
+    >
+      LOADING ...
     </div>
+    <div v-if="error" class="h-12 mx-auto my-auto bg-red-400">
+      {{ error.message }}
+    </div>
+    <div v-else>
+      <ProductInfo
+        :img="`${baseUrl}/assets/${product.termekKep}`"
+        :name="product.termekNev"
+        :description="product.termekLeiras"
+        :price="product.termekAr"
+        :gallery="product.termekGaleria"
+      />
     </div>
   </div>
 </template>
