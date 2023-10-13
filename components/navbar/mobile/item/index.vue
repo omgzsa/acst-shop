e8a1f746d2041adf4bc9db66ebba9ac26b4802ab
omgzsa <zselloandras@gmail.com>
2023-10-13 17:12:38 +0200

    [feat]: close navitem if other opens, needs refactor

--- components/navbar/mobile/item/index.vue
+++ components/navbar/mobile/item/index.vue
@@ -4,17 +4,28 @@ const props = defineProps({
   categories: Array,
 });
 
-// console.log(props.categories);
+// if one of the details is open the others should close
+const closeOtherDetails = (event) => {
+  // console.log(event.target);
+  const details = event.target.closest('details');
+  const detailsList = details.parentElement.querySelectorAll('details');
+  detailsList.forEach((item) => {
+    if (item !== details) {
+      item.removeAttribute('open');
+    }
+  });
+};
 </script>
 
 <template>
-  <details>
+  <details @click="closeOtherDetails">
     <summary class="flex items-center justify-between list-none cursor-pointer">
       <div
-        class="text-2xl font-semibold transition-colors duration-300 cursor-pointer text-dark-100 hover:text-dark-300"
+        class="text-2xl font-semibold uppercase transition-colors duration-300 cursor-pointer text-dark-100 hover:text-dark-300"
       >
         {{ props.name }}
-        <Icon name="mdi:chevron-down" />
+        <!-- <Icon name="mdi:chevron-left" class="mb-1" /> -->
+        <Icon name="mdi:chevron-down" class="mb-1" />
       </div>
     </summary>
 
@@ -29,3 +40,28 @@ const props = defineProps({
     </div>
   </details>
 </template>
+
+<style>
+/* dialog[open] {
+  animation: show 0.3s ease normal;
+  -webkit-animation: show 0.3s ease normal;
+}
+
+@keyframes show {
+  from {
+    transform: translateY(-110%);
+  }
+  to {
+    transform: translateY(0%);
+  }
+}
+
+@-webkit-keyframes show {
+  from {
+    transform: translateY(-110%);
+  }
+  to {
+    transform: translateY(0%);
+  }
+} */
+</style>
