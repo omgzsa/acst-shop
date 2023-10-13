eeed0cd1cdf1dd39687c573241d6db0235df3c04
omgzsa <zselloandras@gmail.com>
2023-10-13 17:14:01 +0200

    removed an unnecessary div that caused layout glitch

--- components/navbar/mobile/item/sub/index.vue
+++ components/navbar/mobile/item/sub/index.vue
@@ -9,29 +9,27 @@ const props = defineProps({
 </script>
 
 <template>
-  <div>
-    <div class="grid gap-y-2">
-      <span
-        ><NuxtLink
-          :to="`${props.link}`"
-          class="px-2 py-1 text-base font-semibold leading-snug tracking-wide transition-colors duration-300 rounded xl:text-lg hover:text-dark-100 hover:bg-accent-100 hover:font-bold"
-          >{{ props.name }}</NuxtLink
-        ></span
+  <div class="grid gap-y-2">
+    <span
+      ><NuxtLink
+        :to="`${props.link}`"
+        class="px-2 py-1 text-base font-semibold leading-snug tracking-wide transition-colors duration-300 rounded xl:text-lg hover:text-dark-100 hover:bg-accent-100 hover:font-bold"
+        >{{ props.name }}</NuxtLink
+      ></span
+    >
+    <ul class="grid gap-y-2">
+      <li
+        v-for="sub in props.subCategories"
+        :key="sub.id"
+        class="text-sm rounded-md hover:font-bold"
       >
-      <ul class="grid gap-y-2">
-        <li
-          v-for="sub in props.subCategories"
-          :key="sub.id"
-          class="text-sm rounded-md hover:font-bold"
+        <NuxtLink
+          :to="`${sub.link}`"
+          class="px-2 py-1 transition-colors duration-300 rounded hover:text-dark-100 hover:bg-accent-100 hover:font-semibold"
         >
-          <NuxtLink
-            :to="`${sub.link}`"
-            class="px-2 py-1 transition-colors duration-300 rounded hover:text-dark-100 hover:bg-accent-100 hover:font-semibold"
-          >
-            {{ sub.name }}
-          </NuxtLink>
-        </li>
-      </ul>
-    </div>
+          {{ sub.name }}
+        </NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
