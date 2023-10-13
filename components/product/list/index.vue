da8a6fa318dc37e9a1895980cf5be765e47c7006
omgzsa <zselloandras@gmail.com>
2023-10-13 17:11:25 +0200

    removed transition

--- components/product/list/index.vue
+++ components/product/list/index.vue
@@ -1,6 +1,5 @@
 <script setup>
 const config = useRuntimeConfig();
-
 const baseUrl = config.public.directusUrl;
 
 defineProps({
@@ -15,18 +14,18 @@ defineProps({
   <div
     class="relative grid grid-cols-2 pb-14 gap-x-4 gap-y-8 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5"
   >
-    <TransitionGroup name="popup" appear>
-      <ProductListItem
-        v-for="product in items"
-        :key="product.id"
-        :id="product.id"
-        :name="product.termekNev"
-        :description="product.termekLeiras"
-        :img="`${baseUrl}/assets/${product.termekKep}`"
-        :category-slug="product?.kapcsolodoKategoria?.slug"
-        :sub-category-slug="product?.kapcsolodoAlKategoria?.slug"
-      />
-    </TransitionGroup>
+    <!-- <TransitionGroup name="popup" appear> -->
+    <ProductListItem
+      v-for="product in items"
+      :key="product.id"
+      :id="product.id"
+      :name="product.termekNev"
+      :description="product.termekLeiras"
+      :img="`${baseUrl}/assets/${product.termekKep}`"
+      :category-slug="product?.kapcsolodoKategoria?.slug"
+      :sub-category-slug="product?.kapcsolodoAlKategoria?.slug"
+    />
+    <!-- </TransitionGroup> -->
   </div>
 </template>
 
