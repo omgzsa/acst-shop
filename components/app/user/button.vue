ad888c62201223ec23e61afe7e6b94fad24780ed
omgzsa <zselloandras@gmail.com>
2023-10-14 17:05:24 +0200

    style update on nav buttons user/cart

--- components/app/user/button.vue
+++ components/app/user/button.vue
@@ -6,7 +6,7 @@ const { defaultTransition } = useTailwindConfig();
   <div>
     <Icon
       name="mdi:account-outline"
-      class="p-1.5 rounded-full w-9 h-9 bg-accent-100 hover:bg-dark-100 hover:text-dark-200 cursor-pointer"
+      class="p-1.5 rounded-full w-9 h-9 bg-accent-100 hover:bg-dark-100 hover:text-accent-100 cursor-pointer hover:p-2"
       :class="defaultTransition"
     />
   </div>
