3eac01f56feb198757e2b33ac76dbfe2365465c5
omgzsa <zselloandras@gmail.com>
2023-10-17 16:57:15 +0200

    tech-spec item

---
+++ components/product/techSpec/item/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+defineProps({
+  name: String,
+});
+</script>
+
+<template>
+  <li class="flex flex-col space-y-1">
+    <p class="text-sm md:text-base">{{ name }}</p>
+    <span class="text-base md:text-lg text-dark-300">
+      <slot />
+    </span>
+  </li>
+</template>
