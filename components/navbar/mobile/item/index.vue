952fd581b84ecd80937a799ee74f2f615e7223d8
omgzsa <zselloandras@gmail.com>
2023-10-17 16:56:22 +0200

    [bug]: details open state isOpen ref are not in sync

--- components/navbar/mobile/item/index.vue
+++ components/navbar/mobile/item/index.vue
@@ -4,8 +4,14 @@ const props = defineProps({
   categories: Array,
 });
 
+/* 
+details animation is not working properly, isOpen is not in sync with the details open/close state
+
+needs further research
+*/
+
 const detailsRef = ref(null);
-const isOpen = ref(true);
+// const isOpen = ref(true);
 
 // doesn't work well, something with open state management
 const toggleDetails = () => {
@@ -30,35 +36,34 @@ const toggleDetails = () => {
         class="text-2xl font-semibold uppercase transition-colors duration-300 cursor-pointer text-dark-100 hover:text-dark-300"
       >
         {{ props.name }}
-        <Icon v-if="!isOpen" name="mdi:chevron-down" class="mb-1" />
-        <Icon v-else name="mdi:chevron-left" class="mb-1" />
+        <Icon name="mdi:chevron-down" class="mb-1" />
+        <!-- <Icon name="mdi:chevron-left" class="mb-1" /> -->
       </div>
     </summary>
 
-    <Transition name="fade" mode="out-in">
-      <div
-        class="grid py-4 transition-all duration-300 rounded opacity-0 gap-y-6 bg-dark-200"
-        :class="{ 'opacity-100 pointer-events-auto': isOpen }"
-      >
-        <NavbarMobileItemSub
-          v-for="category in props.categories"
-          :key="category.id"
-          :name="category.name"
-          :link="category.link"
-          :sub-categories="category.subCategories"
-        />
-      </div>
-    </Transition>
+    <!-- <Transition name="fade" mode="out-in"> -->
+    <div
+      class="grid py-4 transition-all duration-300 rounded gap-y-6 bg-dark-200"
+    >
+      <NavbarMobileItemSub
+        v-for="category in props.categories"
+        :key="category.id"
+        :name="category.name"
+        :link="category.link"
+        :sub-categories="category.subCategories"
+      />
+    </div>
+    <!-- </Transition> -->
   </details>
 </template>
 
 <style>
-.fade-enter-active,
+/* .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.3s;
 }
 
-.fade-enter, .fade-leave-to /* .fade-leave-active in <2.1.8 */ {
+.fade-enter, .fade-leave-to {
   opacity: 0;
-}
+} */
 </style>
