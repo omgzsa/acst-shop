54c26784f6acdf33dd11c4727b97d87e5250112e
omgzsa <zselloandras@gmail.com>
2023-10-18 17:12:27 +0200

    [feat]: tech-spec keys and items

--- components/product/techSpec/index.vue
+++ components/product/techSpec/index.vue
@@ -10,72 +10,201 @@ const { data: techSpec } = await useAsyncData('techSpec', () => {
   return getItemById({
     collection: `${props.specKey}`,
     id: `${props.specId}`,
+    // params: {
+    //   fields: ['.*', `${props.specKey}`],
+    // },
   });
 });
 
-// console.log(techSpec.value);
+console.log(techSpec.value);
 </script>
 
 <template>
   <!-- PRODUCT TECHNICAL SPECIFICATIONS -->
-  <div class="pb-8 space-y-4 border-b site-padding">
+  <div class="pb-8 space-y-4 border-b lg:col-span-3">
     <h2>Technikai adatok</h2>
-    <ul class="space-y-4">
-      <ProductTechSpecItem
+    <ul
+      class="gap-4 space-y-4 sm:space-y-0 sm:grid sm:grid-cols-2 lg:grid-cols-3"
+    >
+      <ProductTechSpecItemNumber
         name="Szállítható kerékpárok"
+        :value="techSpec.szallithato_kerekparok"
         v-if="techSpec.szallithato_kerekparok"
-      >
-        {{ techSpec.szallithato_kerekparok }}
-      </ProductTechSpecItem>
-      <ProductTechSpecItem name="Terhelhetőség" v-if="techSpec.terhelhetoseg">
-        {{ techSpec.terhelhetoseg }}
-      </ProductTechSpecItem>
-      <ProductTechSpecItem name="Méret (HxSZxM)" v-if="techSpec.meret_h_sz_m">
-        {{ techSpec.meret_h_sz_m }}
-      </ProductTechSpecItem>
-      <ProductTechSpecItem
+      />
+
+      <ProductTechSpecItemString
+        name="Terhelhetőség"
+        v-if="techSpec.terhelhetoseg"
+        :value="techSpec.terhelhetoseg"
+      />
+
+      <ProductTechSpecItemString
+        name="1db kerékpár max súlya"
+        v-if="techSpec.egy_kerekpar_max_sulya"
+        :value="techSpec.egy_kerekpar_max_sulya"
+      />
+
+      <ProductTechSpecItemString
+        name="Méret (HxSZxM)"
+        v-if="techSpec.meret_h_sz_m"
+        :value="techSpec.meret_h_sz_m"
+      />
+
+      <ProductTechSpecItemString
         name="Méret összehajtva"
         v-if="techSpec.meret_osszehajtva"
-      >
-        {{ techSpec.meret_osszehajtva }}
-      </ProductTechSpecItem>
-      <ProductTechSpecItem name="Súly" v-if="techSpec.suly">
-        {{ techSpec.suly }}
-      </ProductTechSpecItem>
-      <ProductTechSpecItem
+        :value="techSpec.meret_osszehajtva"
+      />
+
+      <ProductTechSpecItemString
+        name="Súly"
+        v-if="techSpec.suly"
+        :value="techSpec.suly"
+      />
+
+      <ProductTechSpecItemString
+        name="Kerékpárváz méret kompatibilitás"
+        v-if="techSpec.kerekparvaz_meret_kompatibilitas"
+        :value="techSpec.kerekparvaz_meret_kompatibilitas"
+      />
+
+      <ProductTechSpecItemString
+        name="Kerek kerékpárváz méret"
+        v-if="techSpec.kerek_kerekparvaz_meret"
+        :value="techSpec.kerek_kerekparvaz_meret"
+      />
+
+      <ProductTechSpecItemString
+        name="Ovális kerékpárváz méret"
+        v-if="techSpec.ovalis_kerekparvaz_meret"
+        :value="techSpec.ovalis_kerekparvaz_meret"
+      />
+
+      <ProductTechSpecItemNumber
+        name="Max. gumiabroncs szélesség"
+        v-if="techSpec.max_gumiabroncs_szelesseg"
+        :value="techSpec.max_gumiabroncs_szelesseg"
+      />
+
+      <ProductTechSpecItemString
         name="Max. szállítható kerékméret"
         v-if="techSpec.max_szallithato_kerekmeret"
-      >
-        {{ techSpec.max_szallithato_kerekmeret }}
-      </ProductTechSpecItem>
-      <ProductTechSpecItem
+        :value="techSpec.max_szallithato_kerekmeret"
+      />
+
+      <ProductTechSpecItemString
+        name="Max. tengelytávolság"
+        v-if="techSpec.max_tengelytav"
+        :value="techSpec.max_tengelytav"
+      />
+
+      <ProductTechSpecItemString
         name="Kerékpárok közötti távolság"
         v-if="techSpec.kerekparok_kozotti_tavolsag"
-      >
-        {{ techSpec.kerekparok_kozotti_tavolsag }}
-      </ProductTechSpecItem>
-      <ProductTechSpecItem
+        :value="techSpec.kerekparok_kozotti_tavolsag"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.hatso_lampak_rendszamtabla_lathato"
+        name="Hátsó lámpák és rendszámtábla látható kerékpárokkal szerelve"
+        :is-true="!techSpec.hatso_lampak_rendszamtabla_lathato"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.karbonvaz_kompatibilis"
+        name="Karbonváz kompatibilis"
+        :is-true="!techSpec.karbonvaz_kompatibilis"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.THULE_SquareBar_rudra_szerelheto"
+        name="THULE SquareBar rúdra szerelhető"
+        :is-true="!techSpec.THULE_SquareBar_rudra_szerelheto"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.THULE_WingBar_EVO_rudra_szerelheto"
+        name="THULE WingBar EVO rúdra szerelhető"
+        :is-true="!techSpec.THULE_WingBar_EVO_rudra_szerelheto"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.THULE_WingBar_Edge_rudra_szerelheto"
+        name="THULE WingBar Edge rúdra szerelhető"
+        :is-true="!techSpec.THULE_WingBar_Edge_rudra_szerelheto"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.THULE_WingBar_rudra_szerelheto"
+        name="THULE WingBar rúdra szerelhető"
+        :is-true="!techSpec.THULE_WingBar_rudra_szerelheto"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.THULE_ProBar_rudra_szerelheto"
+        name="THULE ProBar rúdra szerelhető"
+        :is-true="!techSpec.THULE_ProBar_rudra_szerelheto"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.THULE_SlideBar_rudra_szerelheto"
+        name="THULE SlideBar rúdra szerelhető"
+        :is-true="!techSpec.THULE_SlideBar_rudra_szerelheto"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.osszecsukhato"
+        name="Összecsukható"
+        :is-true="!techSpec.osszecsukhato"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.dontheto !== false"
+        name="Dönthető"
+        :is-true="!techSpec.dontheto"
+      />
+
+      <ProductTechSpecItemString
         name="Elektromos csatlakozás"
         v-if="techSpec.elektromos_csatlakozas"
-      >
-        {{ techSpec.elektromos_csatlakozas }}
-      </ProductTechSpecItem>
-      <ProductTechSpecItem name="Szín" v-if="techSpec.szin">
-        {{ techSpec.szin }}
-      </ProductTechSpecItem>
-      <ProductTechSpecItem name="Típusszám" v-if="techSpec.tipusszam">
-        {{ techSpec.tipusszam }}
-      </ProductTechSpecItem>
-      <ProductTechSpecItem name="Összecsukható" v-if="techSpec.osszecsukhato">
-        <Icon name="mdi:check" v-if="techSpec.osszecsukhato" />
-        <Icon name="mdi:close" v-else />
-        <div class="hidden">{{ techSpec.osszecsukhato }}</div>
-      </ProductTechSpecItem>
-      <ProductTechSpecItem name="Dönthető" v-if="techSpec.dontheto">
-        <Icon name="mdi:check" v-if="techSpec.dontheto" />
-        <Icon name="mdi:close" v-else />
-        <div class="hidden">{{ techSpec.dontheto }}</div>
-      </ProductTechSpecItem>
+        :value="techSpec.elektromos_csatlakozas"
+      />
+
+      <ProductTechSpecItemString
+        name="Szín"
+        v-if="techSpec.szin"
+        :value="techSpec.szin"
+      />
+
+      <ProductTechSpecItemNumber
+        name="Típusszám"
+        v-if="techSpec.tipusszam"
+        :value="techSpec.tipusszam"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.kiegeszito_lampatest"
+        name="Kiegészítő lámpatest"
+        :is-true="!techSpec.kiegeszito_lampatest"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.eBike_kompatibilis"
+        name="E-bike kompatibilis"
+        :is-true="!techSpec.eBike_kompatibilis"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.rendszamtabla_tarto"
+        name="Rendszámtábla tartó"
+        :is-true="!techSpec.rendszamtabla_tarto"
+      />
+
+      <ProductTechSpecItemBool
+        v-if="techSpec.one_key_system_kompatibilis"
+        name="One Key System kompatibilis"
+        :is-true="!techSpec.one_key_system_kompatibilis"
+      />
     </ul>
   </div>
 </template>
---
+++ components/product/techSpec/item/string.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  name: String,
+  value: String,
+});
+
+// console.log(typeof props.name);
+</script>
+
+<template>
+  <li class="flex flex-col space-y-1">
+    <p class="text-sm md:text-base">{{ props.name }}</p>
+    <span class="text-base md:text-lg text-dark-300">
+      {{ props.value }}
+    </span>
+  </li>
+</template>
