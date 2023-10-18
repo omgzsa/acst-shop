dcb952994f0dc22394681140288d277c438a3b96
omgzsa <zselloandras@gmail.com>
2023-10-18 20:51:16 +0200

    [style]: refactored link styling and hover

--- components/navbar/desktop/item/sub.vue
+++ components/navbar/desktop/item/sub.vue
@@ -9,20 +9,22 @@ const props = defineProps({
 </script>
 
 <template>
-  <ul class="">
-    <li
-      class="px-2 py-1 text-sm rounded-md hover:bg-accent-100 hover:font-bold"
-      :class="defaultTransition"
-    >
-      <NuxtLink class="w-full font-semibold" :to="link">{{ name }}</NuxtLink>
+  <ul class="space-y-1.5">
+    <li>
+      <NuxtLink
+        class="w-full px-2 py-1 text-sm font-semibold rounded-md hover:bg-accent-100 hover:font-bold"
+        :to="link"
+        :class="defaultTransition"
+        >{{ name }}</NuxtLink
+      >
     </li>
-    <li
-      v-for="sub in props.subCategories"
-      :key="sub.id"
-      class="px-2 py-1 text-sm rounded-md hover:bg-accent-100 hover:font-bold"
-      :class="defaultTransition"
-    >
-      <NuxtLink :to="sub.link">{{ sub.name }}</NuxtLink>
+    <li v-for="sub in props.subCategories" :key="sub.id">
+      <NuxtLink
+        class="px-2 py-1 text-sm rounded-md hover:bg-accent-100 hover:font-bold"
+        :to="sub.link"
+        :class="defaultTransition"
+        >{{ sub.name }}</NuxtLink
+      >
     </li>
   </ul>
 </template>
