8e167cd29d151ffb41effb49defebd44990bf3fb
omgzsa <zselloandras@gmail.com>
2023-10-30 15:06:58 +0100

    [style]: added transition group animation for filters

--- components/product/list/index.vue
+++ components/product/list/index.vue
@@ -14,33 +14,33 @@ defineProps({
   <div
     class="relative grid grid-cols-2 pb-14 gap-x-4 gap-y-8 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5"
   >
-    <!-- <TransitionGroup name="popup" appear> -->
-    <ProductListItem
-      v-for="product in items"
-      :key="product.id"
-      :id="product.id"
-      :name="product.termekNev"
-      :description="product.termekLeiras"
-      :img="`${baseUrl}/assets/${product.termekKep}`"
-      :category-slug="product?.kapcsolodoKategoria?.slug"
-      :sub-category-slug="product?.kapcsolodoAlKategoria?.slug"
-    />
-    <!-- </TransitionGroup> -->
+    <TransitionGroup name="popup" appear>
+      <ProductListItem
+        v-for="product in items"
+        :key="product.id"
+        :id="product.id"
+        :name="product.termekNev"
+        :description="product.termekLeiras"
+        :img="`${baseUrl}/assets/${product.termekKep}`"
+        :category-slug="product?.kapcsolodoKategoria?.slug"
+        :sub-category-slug="product?.kapcsolodoAlKategoria?.slug"
+      />
+    </TransitionGroup>
   </div>
 </template>
 
 <style>
-/* .popup-move,
+.popup-move,
 .popup-enter-active,
 .popup-leave-active {
-  transition: all 0.5s ease;
-} */
+  transition: all 0.3s ease;
+}
 
-/* .popup-enter-from,
+.popup-enter-from,
 .popup-leave-to {
   opacity: 0;
-  transform: translateY(-50px);
-} */
+  transform: translateY(-20px);
+}
 
 /* .popup-leave-active {
   position: absolute;
