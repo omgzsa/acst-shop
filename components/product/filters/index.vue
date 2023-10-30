9a352649dece2615b201c4f2efd98c8bb838fb7a
omgzsa <zselloandras@gmail.com>
2023-10-30 10:42:41 +0100

    [refactor]: refactored v-model on filters, emit array to parent

--- components/product/filters/index.vue
+++ components/product/filters/index.vue
@@ -1,17 +1,34 @@
 <script setup>
-// const { defaultTransition } = useTailwindConfig();
-
-defineProps({
-  quantity: {
-    type: Number,
-    required: true,
-  },
+const availableBikeFilter = computed(() => {
+  return [2, 3, 4];
 });
+
+const props = defineProps({
+  quantity: Number,
+  modelValue: Array,
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const filterByBikeNumber = (event) => {
+  const { value } = event.target;
+
+  if (event.target.checked) {
+    emit('update:modelValue', [...props.modelValue, Number(value)]);
+  } else {
+    emit(
+      'update:modelValue',
+      props.modelValue.filter((item) => item !== Number(value))
+    );
+  }
+};
 </script>
 
 <template>
-  <div class="grid grid-cols-6 py-3 border-t-2 border-b-2 border-dark-200">
-    <details class="col-span-4 sm:col-span-5">
+  <div
+    class="relative grid grid-cols-6 py-3 border-t-2 border-b-2 border-dark-200"
+  >
+    <details class="col-span-6">
       <summary
         class="flex items-center justify-between list-none cursor-pointer"
       >
@@ -20,8 +37,38 @@ defineProps({
           <span>Szűrés</span>
         </div>
       </summary>
-      <p class="pt-4">FILTERS</p>
+      <details
+        class="overflow-hidden rounded [&_summary::-webkit-details-marker]:hidden group"
+      >
+        <summary
+          class="flex items-center justify-between gap-2 pt-4 text-gray-900 transition bg-white cursor-pointer"
+        >
+          <span class="group-hover:font-bold group-hover:text-dark-100">
+            Szállítható kerékpárok
+          </span>
+
+          <span class="transition group-open:-rotate-180">
+            <Icon name="mdi:chevron-down" class="mb-1" size="18" />
+          </span>
+        </summary>
+        <div class="py-2 space-y-1">
+          <label
+            v-for="option in availableBikeFilter"
+            :key="option"
+            class="flex items-center gap-x-2"
+            for="filterMaxBikeNumber"
+          >
+            <input
+              id="filterMaxBikeNumber"
+              type="checkbox"
+              :value="option"
+              @change="filterByBikeNumber"
+            />
+            <p>{{ option }}</p>
+          </label>
+        </div>
+      </details>
     </details>
-    <span class="col-span-2 ml-auto sm:col-span-1">{{ quantity }} termék</span>
+    <span class="absolute right-0 top-3">{{ quantity }} termék</span>
   </div>
 </template>
