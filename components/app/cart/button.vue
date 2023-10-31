ed836da8f1f377ce5a2402370ce4d0458707a7ae
omgzsa <zselloandras@gmail.com>
2023-10-31 22:12:55 +0100

    [feat]: added itemCount to cart button

--- components/app/cart/button.vue
+++ components/app/cart/button.vue
@@ -1,13 +1,23 @@
 <script setup>
+import { useCartStore } from '@/stores/cart';
 const { defaultTransition } = useTailwindConfig();
+
+const cartStore = useCartStore();
 </script>
 
 <template>
-  <div>
-    <Icon
-      name="mdi:cart-outline"
-      class="p-1.5 rounded-full w-9 h-9 bg-accent-100 hover:bg-dark-100 hover:text-accent-100 cursor-pointer hover:p-2"
-      :class="defaultTransition"
-    />
-  </div>
+  <NuxtLink to="/kosar">
+    <div class="relative">
+      <Icon
+        name="mdi:cart-outline"
+        class="p-1.5 rounded-full w-9 h-9 bg-accent-100 hover:bg-dark-100 hover:text-accent-100 cursor-pointer hover:p-2"
+        :class="defaultTransition"
+      />
+      <span
+        v-if="cartStore.itemCount"
+        class="absolute flex items-center justify-center w-5 h-5 pt-0.5 text-xs font-semibold text-white bg-red-600 border rounded-full -top-2 -right-2"
+        >{{ cartStore.itemCount }}</span
+      >
+    </div>
+  </NuxtLink>
 </template>
