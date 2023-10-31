92b1491800af77ffa4e4e9d0bb27331abcd2ec00
omgzsa <zselloandras@gmail.com>
2023-10-31 14:10:49 +0100

    [style]: style and spacing changes

--- components/navbar/desktop/item/sub.vue
+++ components/navbar/desktop/item/sub.vue
@@ -10,21 +10,22 @@ const props = defineProps({
 
 <template>
   <ul>
-    <li class="tracking-wider rounded-md hover:font-semibold">
+    <li class="mb-2 tracking-wider border-b rounded-md hover:font-semibold">
       <NuxtLink
-        class="block px-4 py-2 text-sm font-bold text-gray-600 rounded-md hover:bg-accent-100/70 hover:text-dark-100"
+        class="block px-3 py-1.5 text-sm font-bold text-gray-600 rounded-md hover:bg-accent-100/70 hover:text-dark-100"
         :to="link"
         :class="defaultTransition"
         >{{ name }}</NuxtLink
       >
     </li>
+
     <li
       class="tracking-wider rounded-md hover:font-medium"
       v-for="sub in props.subCategories"
       :key="sub.id"
     >
       <NuxtLink
-        class="block px-4 py-2 text-sm text-gray-600 rounded-md hover:bg-accent-100/70 hover:text-dark-100"
+        class="block px-3 py-1.5 text-sm text-gray-600 rounded-md hover:bg-accent-100/70 hover:text-dark-100"
         :to="sub.link"
         :class="defaultTransition"
         >{{ sub.name }}</NuxtLink
