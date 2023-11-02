b6f954a426773b677aaf1744cfd2c34c65bfe310
omgzsa <zselloandras@gmail.com>
2023-11-02 11:14:21 +0100

    app-button own component

---
+++ components/app/button.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { defaultTransition } = useTailwindConfig();
+
+defineProps({
+  label: String,
+  to: String,
+});
+</script>
+
+<template>
+  <button
+    class="px-4 py-2 space-x-2 font-semibold border rounded-md shadow-md border-accent-100 duration-400 bg-accent-100 text-dark-100 hover:bg-dark-100 hover:text-white hover:shadow-lg"
+    :class="defaultTransition"
+  >
+    <NuxtLink :to="to">
+      <span class="text-base leading-snug tracking-wide xl:text-lg">
+        {{ label }}
+      </span></NuxtLink
+    >
+  </button>
+</template>
