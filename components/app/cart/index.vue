3b009c6655f0e061176ae549085792f668e706e9
omgzsa <zselloandras@gmail.com>
2023-11-02 11:14:53 +0100

    cart

--- components/app/cart/index.vue
+++ components/app/cart/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+const { defaultTransition } = useTailwindConfig();
 import { useCartStore } from '@/stores/cart';
 
 const cartStore = useCartStore();
@@ -6,21 +7,29 @@ const cartStore = useCartStore();
 
 <template>
   <section>
-    <div class="py-10 site-padding">
-      <div class="max-w-3xl mx-auto">
-        <header class="text-center">
-          <h1 class="text-xl font-bold text-gray-900 sm:text-3xl">Kosár</h1>
+    <div class="min-h-[80vh] py-6 lg:py-10 site-padding">
+      <div class="max-w-xl mx-auto">
+        <header class="space-y-2">
+          <h1 class="">Kosár</h1>
+          <p class="inline-block mr-1">Valami még kimaradt?</p>
+          <span class="underline">Folytasd a vásárlást itt.</span>
         </header>
 
         <div class="mt-8">
-          <ul v-if="!cartStore.isEmpty" class="space-y-4">
+          <ul v-if="!cartStore.isEmpty">
             <AppCartItem
               v-for="item in cartStore.items"
-              :key="item.id"
-              :id="item.id"
+              :key="item"
               :name="item.name"
               :img="item.img"
+              :price="item.price"
+              :quantity="item.quantity"
             />
+            <!-- <AppCartItem
+              v-for="(items, name) in cartStore.grouped"
+              :item="items[0]"
+              :key="name"
+            /> -->
           </ul>
 
           <div v-else>
@@ -28,8 +37,8 @@ const cartStore = useCartStore();
           </div>
 
           <!-- SUMMERY  -->
-          <!-- <div class="flex justify-end pt-8 mt-8 border-t border-gray-100">
-            <div class="w-screen max-w-lg space-y-4">
+          <div class="flex justify-end pt-8">
+            <div class="w-full max-w-sm space-y-4">
               <dl class="space-y-0.5 text-sm text-gray-700">
                 <div class="flex justify-between">
                   <dt>Subtotal</dt>
@@ -47,44 +56,25 @@ const cartStore = useCartStore();
                 </div>
 
                 <div class="flex justify-between !text-base font-medium">
-                  <dt>Total</dt>
-                  <dd>£200</dd>
+                  <dt>Teljes összeg:</dt>
+                  <dd>{{ cartStore.cartTotal }} Ft</dd>
                 </div>
               </dl>
 
-              <div class="flex justify-end">
-                <span
-                  class="inline-flex items-center justify-center rounded-full bg-indigo-100 px-2.5 py-0.5 text-indigo-700"
+              <div class="flex justify-end space-x-2">
+                <button
+                  class="px-4 py-2 space-x-2 font-semibold border rounded-md shadow-md hover:shadow-lg hover:bg-dark-300 hover:text-dark-200"
+                  :class="defaultTransition"
+                  @click="cartStore.$reset()"
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class="-ms-1 me-1.5 h-4 w-4"
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
-                    />
-                  </svg>
-
-                  <p class="text-xs whitespace-nowrap">2 Discounts Applied</p>
-                </span>
-              </div>
-
-              <div class="flex justify-end">
-                <a
-                  href="#"
-                  class="block px-5 py-3 text-sm text-gray-100 transition bg-gray-700 rounded hover:bg-gray-600"
-                >
-                  Checkout
-                </a>
+                  <span class="text-base leading-snug tracking-wide xl:text-lg">
+                    Kosár kiürítése
+                  </span>
+                </button>
+                <AppButton label="Fizetés" to="/" />
               </div>
             </div>
-          </div> -->
+          </div>
         </div>
       </div>
     </div>
