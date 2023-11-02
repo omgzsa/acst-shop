620bcd01a283d194478eb29fe8da086308dfa3cc
omgzsa <zselloandras@gmail.com>
2023-11-02 11:16:40 +0100

    pinia cartStore

--- stores/cart.js
+++ stores/cart.js
@@ -1,23 +1,43 @@
-export const useCartStore = defineStore('cartStore', () => {
-  // state = data
-  const items = ref([]);
+import { defineStore } from 'pinia';
+// import { groupBy } from 'lodash-es';
 
-  // actions = functions
+// State
+export const useCartStore = defineStore('cartStore', () => {
+  const items = ref([]);
+  // const AFA = 1.27;
+
+  // Actions
+  // push item to array, or increase quantity.value if already exists
   function addItems(item) {
-    items.value.push({ ...item });
+    const index = items.value.findIndex((i) => i.id === item.id);
+    if (index === -1) {
+      items.value.push({ ...item, quantity: 1 });
+    } else {
+      items.value[index].quantity++;
+    }
   }
 
-  // getters = computed
+  function $reset() {
+    items.value = [];
+  }
+
+  // Getters
   const itemCount = computed(() => items.value.length);
   const isEmpty = computed(() => itemCount.value === 0);
-  const grouped = computed(() => {
-    //
-  });
+  // const grouped = computed(() => groupBy(items.value, (item) => item.name));
+  // const groupCount = computed((name) => items.value[name].length);
+  const cartTotal = computed(() =>
+    items.value.reduce((acc, item) => acc + item.price * item.quantity, 0)
+  );
 
   return {
     items,
     itemCount,
     isEmpty,
+    cartTotal,
+    // grouped,
+    // groupCount,
+    $reset,
     addItems,
   };
 });
