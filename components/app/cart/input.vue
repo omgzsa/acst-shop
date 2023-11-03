deef331f39f762cdeee5d968f6a808dc4034856d
omgzsa <zselloandras@gmail.com>
2023-11-03 15:14:55 +0100

    update model-value to be in sync with local-value

--- components/app/cart/input.vue
+++ components/app/cart/input.vue
@@ -1,13 +1,19 @@
 <script setup>
-defineProps({
-  modelValue: { type: [Number, String], default: 1 },
+const props = defineProps({
+  modelValue: { type: [Number, String], default: 0 },
 });
 
-const emit = defineEmits(['update:modelValue', 'input']);
-const updateValue = (value) => {
-  if (value !== '') {
-    emit('update:modelValue', value);
+const localValue = ref(props.modelValue);
+watch(
+  () => props.modelValue,
+  (value) => {
+    localValue.value = value;
   }
+);
+
+const emit = defineEmits(['update:model-value']);
+const updateValue = () => {
+  emit('update:model-value', localValue.value);
 };
 </script>
 
@@ -16,11 +22,9 @@ const updateValue = (value) => {
     <label for="Line1Qty" class="sr-only"> darab </label>
 
     <input
-      :value="modelValue"
-      @input="updateValue($event.target.value)"
+      v-model="localValue"
       type="number"
       min="1"
-      value="1"
       id="Line1Qty"
       class="h-8 w-12 rounded border-gray-200 bg-gray-50 p-0 text-center text-xs text-gray-600 [-moz-appearance:_textfield] focus:outline-none [&::-webkit-inner-spin-button]:m-0 [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-outer-spin-button]:appearance-none"
     />
