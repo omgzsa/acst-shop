e236792f81ced901463bd2b8104b19f264bae841
omgzsa <zselloandras@gmail.com>
2023-11-11 16:49:31 +0100

    test

--- pages/szallitas-es-fizetes/index.vue
+++ pages/szallitas-es-fizetes/index.vue
@@ -4,7 +4,17 @@
 
 <template>
   <div class="bg-white">
-    <div class="site-padding">form</div>
+    <div class="py-10 site-padding">
+      <FormKit
+        label="Név"
+        name="fullName"
+        type="text"
+        help="Kérjük adja meg a teljes nevét"
+        validation="required"
+        value="@useFormkit"
+        prefix-icon="avatarMan"
+      />
+    </div>
   </div>
 </template>
 
