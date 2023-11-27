7f1b6c69ff514a2f74649183a1c42d4b27ea3ec6
omgzsa <zselloandras@gmail.com>
2023-11-27 16:54:57 +0100

    basic app button w/ styles

--- components/app/button.vue
+++ components/app/button.vue
@@ -8,14 +8,14 @@ defineProps({
 </script>
 
 <template>
-  <button
-    class="px-4 py-2 space-x-2 font-semibold border rounded-md shadow-md border-accent-100 duration-400 bg-accent-100 text-dark-100 hover:bg-dark-100 hover:text-white hover:shadow-lg"
-    :class="defaultTransition"
-  >
-    <NuxtLink :to="to">
+  <NuxtLink :to="to">
+    <button
+      class="px-4 py-2 space-x-2 font-semibold border rounded-md shadow-md border-accent-100 duration-400 bg-accent-100 text-dark-100 hover:bg-dark-100 hover:text-white hover:shadow-lg"
+      :class="defaultTransition"
+    >
       <span class="text-base leading-snug tracking-wide xl:text-lg">
         {{ label }}
-      </span></NuxtLink
-    >
-  </button>
+      </span>
+    </button>
+  </NuxtLink>
 </template>
