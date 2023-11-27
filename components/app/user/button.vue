0bd7c7d58318cd498eb91041c0c51f5a316099e7
omgzsa <zselloandras@gmail.com>
2023-11-27 16:52:10 +0100

    Fix user button link

--- components/app/user/button.vue
+++ components/app/user/button.vue
@@ -3,11 +3,11 @@ const { defaultTransition } = useTailwindConfig();
 </script>
 
 <template>
-  <div>
+  <NuxtLink to="/profil">
     <Icon
       name="mdi:account-outline"
       class="p-1.5 rounded-full w-9 h-9 bg-accent-100 hover:bg-dark-100 hover:text-accent-100 cursor-pointer hover:p-2"
       :class="defaultTransition"
     />
-  </div>
+  </NuxtLink>
 </template>
