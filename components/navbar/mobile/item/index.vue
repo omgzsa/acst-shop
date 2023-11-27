084e22724f7ddbe37f135b81c736855a98430e35
omgzsa <zselloandras@gmail.com>
2023-11-27 16:53:51 +0100

    removed not working code for details toggle

--- components/navbar/mobile/item/index.vue
+++ components/navbar/mobile/item/index.vue
@@ -3,36 +3,10 @@ const props = defineProps({
   name: String,
   categories: Array,
 });
-
-/* 
-details animation is not working properly, isOpen is not in sync with the details open/close state
-
-needs further research
-*/
-
-const detailsRef = ref(null);
-// const isOpen = ref(true);
-
-// doesn't work well, something with open state management
-const toggleDetails = () => {
-  const details = detailsRef.value;
-  const detailsList = details.parentElement.children;
-
-  for (let i = 0; i < detailsList.length; i++) {
-    const item = detailsList[i];
-    if (item.open && item !== details) {
-      item.removeAttribute('open');
-    }
-  }
-};
-
-// console.log();
 </script>
 
 <template>
   <details
-    @click="toggleDetails"
-    ref="detailsRef"
     class="overflow-hidden rounded [&_summary::-webkit-details-marker]:hidden"
   >
     <summary class="flex items-center justify-between list-none cursor-pointer">
@@ -41,7 +15,6 @@ const toggleDetails = () => {
       >
         {{ props.name }}
         <Icon name="mdi:chevron-down" class="mb-1" />
-        <!-- <Icon name="mdi:chevron-left" class="mb-1" /> -->
       </div>
     </summary>
 
