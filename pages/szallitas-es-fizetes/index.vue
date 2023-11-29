7e4c2890dc1df4419ca81c82460e5add4578b058
omgzsa <zselloandras@gmail.com>
2023-11-29 15:54:52 +0100

    checkout main

--- pages/szallitas-es-fizetes/index.vue
+++ pages/szallitas-es-fizetes/index.vue
@@ -1,21 +1,57 @@
 <script setup>
-//
+// const value = ref('GLS');
 </script>
 
 <template>
   <div class="bg-white">
     <div class="py-10 site-padding">
-      <FormKit
-        label="Név"
-        name="fullName"
-        type="text"
-        help="Kérjük adja meg a teljes nevét"
-        validation="required"
-        value="@useFormkit"
-        prefix-icon="avatarMan"
-      />
+      <FormKit type="form" :actions="false" #default="{ value }">
+        <FormKit
+          type="multi-step"
+          tab-style="progress"
+          :classes="{
+            outer: 'border-0 shadow-none',
+          }"
+        >
+          <FormKit type="step" name="Szállítási cím">
+            <CheckoutStepOne />
+          </FormKit>
+
+          <FormKit type="step" name="Szállítás módja">
+            <CheckoutStepTwo />
+          </FormKit>
+
+          <FormKit type="step" name="Számlázási cím">
+            <CheckoutStepThree />
+          </FormKit>
+
+          <template #stepNext>
+            <FormKit type="submit" />
+          </template>
+        </FormKit>
+        <pre class="mt-10">
+          {{ value }}
+        </pre>
+      </FormKit>
+      <!-- <div class="space-y-4">
+        <FormKit type="text" name="Név" label="Név" placeholder="someValue" />
+        <FormKit
+          v-model="value"
+          type="radio"
+          label="Preferred transportation"
+          :options="['GLS', 'MPL', 'Foxpost', 'Személyes átvétel']"
+          help="How do you like to get around?"
+        />
+      </div> -->
     </div>
   </div>
 </template>
 
-<style></style>
+<style>
+/* .formkit-outer[data-type='multi-step']
+  > [data-tab-style='progress']
+  > .formkit-steps {
+  border: 0;
+  box-shadow: none;
+} */
+</style>
