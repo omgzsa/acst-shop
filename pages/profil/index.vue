571c04343a6da9c50d8fab46091bf278430c00c9
omgzsa <zselloandras@gmail.com>
2023-11-29 15:55:17 +0100

    profile main

--- pages/profil/index.vue
+++ pages/profil/index.vue
@@ -1,9 +1,25 @@
 <script setup>
+const { token, refreshTokens, refreshToken, expires } = useDirectusToken();
+const { logout } = useDirectusAuth();
 definePageMeta({
-  middleware: ['auth'],
+  middleware: ['auth', 'logged-in'],
 });
+
+const user = useDirectusUser();
 </script>
 
 <template>
-  <div class="bg-white">FELHASZNÁLÓ PROFIL</div>
+  <div class="bg-white">
+    <div class="site-padding">
+      <h1>Profil</h1>
+      {{ refreshToken }}
+    </div>
+    <button
+      class="px-6 py-3 text-white bg-gray-500 rounded hover:bg-gray-800"
+      @click="logout()"
+    >
+      logout
+    </button>
+    <pre>{{ user }}</pre>
+  </div>
 </template>
