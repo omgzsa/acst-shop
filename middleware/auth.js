4d51bfe4a3065372ae105f2a80e7da6348171ff0
omgzsa <zselloandras@gmail.com>
2023-12-01 17:00:14 +0100

    auth middleware sample

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,11 @@
-export default defineNuxtRouteMiddleware(async (to, from) => {
-  const { fetchUser, setUser } = useDirectusAuth();
-  const user = useDirectusUser();
-  if (!user.value) {
-    const user = await fetchUser();
-    setUser(user.value);
-  }
-  if (!user.value) {
-    return navigateTo('/login');
-  }
-});
+// export default defineNuxtRouteMiddleware(async (to, from) => {
+//   const { fetchUser, setUser } = useDirectusAuth();
+//   const user = useDirectusUser();
+//   if (!user.value) {
+//     const user = await fetchUser();
+//     setUser(user.value);
+//   }
+//   if (!user.value) {
+//     return navigateTo('/login');
+//   }
+// });
