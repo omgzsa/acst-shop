fd1c59766ae18358299b57958fd7adfa971a6baf
omgzsa <zselloandras@gmail.com>
2023-12-03 22:21:57 +0100

    profil/info subpage

---
+++ pages/profil/info.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+const user = useDirectusUser();
+</script>
+
+<template>
+  <div class="bg-white">
+    <div class="site-padding">
+      <h1>Profil info</h1>
+      <!-- <div>
+        <h2 class="text-base font-normal uppercase text-dark-300">
+          first name
+        </h2>
+        <p>
+          {{ user!.first_name }}
+        </p>
+      </div>
+      <div>
+        <h2 class="text-base font-normal uppercase text-dark-300">last name</h2>
+        <p>
+          {{ user!.last_name }}
+        </p>
+      </div>
+      <div>
+        <h2 class="text-base font-normal uppercase text-dark-300">email</h2>
+        <p>
+          {{ user!.email }}
+        </p>
+      </div> -->
+    </div>
+  </div>
+</template>
