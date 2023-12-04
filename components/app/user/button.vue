75c159ec7386f40fae4ac051d9dd361bc83b4554
omgzsa <zselloandras@gmail.com>
2023-12-04 20:56:57 +0100

    link update

--- components/app/user/button.vue
+++ components/app/user/button.vue
@@ -3,7 +3,7 @@ const { defaultTransition } = useTailwindConfig();
 </script>
 
 <template>
-  <NuxtLink to="/profil">
+  <NuxtLink to="/profil/info">
     <Icon
       name="mdi:account-outline"
       class="p-1.5 rounded-full w-9 h-9 bg-accent-100 hover:bg-dark-100 hover:text-accent-100 cursor-pointer hover:p-2"
