90df2d7661d83f8b5f9eb1a3a2ae1177d6416e56
omgzsa <zselloandras@gmail.com>
2023-12-04 20:58:53 +0100

    removed formkit stuff

--- pages/szallitas-es-fizetes/index.vue
+++ pages/szallitas-es-fizetes/index.vue
@@ -5,53 +5,9 @@
 <template>
   <div class="bg-white">
     <div class="py-10 site-padding">
-      <FormKit type="form" :actions="false" #default="{ value }">
-        <FormKit
-          type="multi-step"
-          tab-style="progress"
-          :classes="{
-            outer: 'border-0 shadow-none',
-          }"
-        >
-          <FormKit type="step" name="Szállítási cím">
-            <CheckoutStepOne />
-          </FormKit>
-
-          <FormKit type="step" name="Szállítás módja">
-            <CheckoutStepTwo />
-          </FormKit>
-
-          <FormKit type="step" name="Számlázási cím">
-            <CheckoutStepThree />
-          </FormKit>
-
-          <template #stepNext>
-            <FormKit type="submit" />
-          </template>
-        </FormKit>
-        <pre class="mt-10">
-          {{ value }}
-        </pre>
-      </FormKit>
-      <!-- <div class="space-y-4">
-        <FormKit type="text" name="Név" label="Név" placeholder="someValue" />
-        <FormKit
-          v-model="value"
-          type="radio"
-          label="Preferred transportation"
-          :options="['GLS', 'MPL', 'Foxpost', 'Személyes átvétel']"
-          help="How do you like to get around?"
-        />
-      </div> -->
+      <h1>szállítás és fizetés</h1>
     </div>
   </div>
 </template>
 
-<style>
-/* .formkit-outer[data-type='multi-step']
-  > [data-tab-style='progress']
-  > .formkit-steps {
-  border: 0;
-  box-shadow: none;
-} */
-</style>
+<style></style>
