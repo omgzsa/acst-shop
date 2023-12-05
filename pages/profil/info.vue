95b0243a0f379dc3fe16f9dd66c53b29e6a38fad
omgzsa <zselloandras@gmail.com>
2023-12-05 17:18:51 +0100

    feat: user/info/ page after login w/ redirect

--- pages/profil/info.vue
+++ pages/profil/info.vue
@@ -1,46 +1,57 @@
 <script lang="ts" setup>
-// import { useAuthStore } from '@/stores/auth';
+import { useAuthStore } from '@/stores/auth';
 // const router = useRouter();
 const user = useDirectusUser();
-const auth = useDirectusAuth();
-
-// const store = useAuthStore();
-
-// const handleLogout = () => {
-//   if (!user.value) return console.log('Nincs bejelentkezve!');
-//   store.userLogout();
-// };
+const store = useAuthStore();
 
 definePageMeta({
   middleware: ['auth'],
 });
-console.log(auth);
+
+/*
+TODOS:
+
+- Middleware routing still needs polishing
+- can start user navigation, needs research on hydration mismatch error
+*/
 </script>
 
 <template>
-  <div class="bg-white">
-    <div class="site-padding">
+  <div class="bg-white" v-if="user">
+    <div class="py-16 site-padding">
       <h1>Profil info</h1>
-      <div>
-        <h2 class="text-base font-normal uppercase text-dark-300">
-          first name
-        </h2>
-        <p>
-          {{ user!.first_name }}
-        </p>
+      <div class="flex justify-between">
+        <div>
+          <h2 class="text-base font-normal uppercase text-dark-300">
+            first name
+          </h2>
+          <p>
+            {{ user?.first_name }}
+          </p>
+        </div>
+        <div>
+          <h2 class="text-base font-normal uppercase text-dark-300">
+            last name
+          </h2>
+          <p>
+            {{ user?.last_name }}
+          </p>
+        </div>
+        <div>
+          <h2 class="text-base font-normal uppercase text-dark-300">email</h2>
+          <p>
+            {{ user?.email }}
+          </p>
+        </div>
       </div>
-      <div>
-        <h2 class="text-base font-normal uppercase text-dark-300">last name</h2>
-        <p>
-          {{ user!.last_name }}
-        </p>
-      </div>
-      <div>
-        <h2 class="text-base font-normal uppercase text-dark-300">email</h2>
-        <p>
-          {{ user!.email }}
-        </p>
-      </div>
+      <!-- HYDRATION MISMATCH ERROR, NESTING INVALID COMPONENTS -->
+      <!-- <ProfileNav /> -->
+      <button
+        class="px-4 py-2 mt-4 font-bold text-white bg-red-500 rounded hover:bg-red-600"
+        @click="store.userLogout"
+      >
+        logout
+      </button>
     </div>
   </div>
 </template>
