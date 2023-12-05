8aad8f371632ee6ab8e710b415985bf670cd5330
omgzsa <zselloandras@gmail.com>
2023-12-05 17:19:34 +0100

    update error page

--- error.vue
+++ error.vue
@@ -1,19 +1,19 @@
 <script setup>
 const { defaultTransition } = useTailwindConfig();
 
-defineProps(['error']);
+const props = defineProps(['error']);
+
+// console.log(props);
 
 const handleClearError = () => clearError({ redirect: '/' });
 </script>
 
 <template>
-  <div class="site-padding">
-    <div
-      class="grid max-w-sm p-10 mx-auto text-center border border-dark-100 place-items-center"
-    >
-      <p class="text-6xl font-bold mt-7">{{ error.statusCode }}</p>
-      <p class="text-2xl mt-7">Oooops...</p>
-      <p class="mt-7">{{ error.message }}</p>
+  <div class="grid items-center h-[85vh] site-padding">
+    <div class="max-w-md p-16 m-auto space-y-8 border border-dark-100">
+      <p class="text-6xl font-bold">{{ error.statusCode }}</p>
+      <p class="text-2xl">Oooops... az oldal amit keresel nem létezik.</p>
+      <p class="">{{ error.message }}</p>
       <button
         :class="defaultTransition"
         class="px-4 py-2 border mt-7 border-dark-100 hover:bg-dark-100 hover:text-white"
