aa4425aaab7980edf49ee34dd1a16ed4909381c1
omgzsa <zselloandras@gmail.com>
2023-12-05 17:22:48 +0100

    directus autoFetch test?

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,11 @@ export default defineNuxtConfig({
   },
   directus: {
     url: process.env.NUXT_DIRECTUS_URL,
-    devtools: true,
+    autoFetch: true,
+    // token: {
+    //   required: true,
+    //   type: 'Bearer',
+    // },
   },
   pinia: {
     autoImports: ['defineStore', 'acceptHMRUpdate'],
