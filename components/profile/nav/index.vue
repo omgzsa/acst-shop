b7aa5db24f3621a3e6a22cafc9d82e1e9c7568e4
omgzsa <zselloandras@gmail.com>
2023-12-07 12:52:37 +0100

    profile nav wrapper component

--- components/profile/nav/index.vue
+++ components/profile/nav/index.vue
@@ -1,12 +1,26 @@
+<script lang="ts" setup>
+import { useAuthStore } from '@/stores/auth';
+const store = useAuthStore();
+
+const userNav = ref([
+  { id: 1, name: 'Információ', key: 'info', path: '/profil/info' },
+  { id: 2, name: 'Rendeléseim', key: 'rendeles', path: '/profil/rendeles' },
+  // { id: 3, name: 'Kijelentkezés', key: 'kijelentkezes'  },
+]);
+</script>
+
 <template>
-  <div>
-    profie navlist
-    <p>
-      <ProfileNavItem />
-    </p>
+  <div class="bg-white">
+    <ul class="space-x-2 space-y-2 text-xl sm:space-x-0 text-dark-300">
+      <ProfileNavItem :user-nav="userNav" />
+      <button
+        class="text-base leading-snug tracking-wide xl:text-lg hover:text-red-700 hover:font-semibold"
+        @click="store.userLogout"
+      >
+        Kijelentkezés
+      </button>
+    </ul>
   </div>
 </template>
-
-<script lang="ts" setup></script>
 
 <style></style>
