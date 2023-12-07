bb602c230674bc9d6eb1688ca4271483d4d80591
omgzsa <zselloandras@gmail.com>
2023-12-07 12:50:41 +0100

    added profil/order page

---
+++ pages/profil/rendeles.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+definePageMeta({
+  middleware: ['auth'],
+  // key: 'rendeles',
+  // layout: 'user',
+});
+</script>
+<template>
+  <div class="bg-white">
+    <div class="py-10 site-padding">
+      <h1>Rendeléseim</h1>
+      <ProfileNav />
+    </div>
+  </div>
+</template>
+
+<style></style>
