fc8c3da99816abecbad82245a032df00b8b10898
omgzsa <zselloandras@gmail.com>
2023-12-07 12:52:49 +0100

    profile/nav-item comp

--- components/profile/nav/item.vue
+++ components/profile/nav/item.vue
@@ -1,7 +1,21 @@
+<script setup>
+const route = useRoute();
+defineProps({
+  userNav: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
 <template>
-  <div>// nav item</div>
+  <li v-for="nav in userNav" :key="nav">
+    <NuxtLink
+      :to="`/profil/${nav.key}`"
+      class="block hover:text-dark-100 hover:font-semibold"
+      :class="{ 'text-dark-100 font-semibold': route.path === nav.path }"
+      >{{ nav.name }}</NuxtLink
+    >
+  </li>
 </template>
-
-<script lang="ts" setup></script>
 
 <style></style>
