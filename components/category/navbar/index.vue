46cd51ebacce8dcd9dddba51660d614e6f7ea20b
omgzsa <zselloandras@gmail.com>
2023-12-13 11:42:56 +0100

    added new category to computed thumb/name

--- components/category/navbar/index.vue
+++ components/category/navbar/index.vue
@@ -1,23 +1,52 @@
 <script setup>
+const props = defineProps({
+  categorySlug: String,
+});
 const { getItems } = useDirectusItems();
 
 const { path, params } = useRoute();
 
 const allBikeRacksThumbnail = '59cb511b-7e6e-4263-83d8-49091f9bcb03';
+const allRoofRacksThumbnail = 'cf6c6827-6fb1-4227-903b-794fb3203d99';
+const allRoofBoxesThumbnail = '664346f5-6579-4434-a2ee-ed03585dbb37';
 
-const { data: kerekpartartokAlKategoriak } = await useAsyncData(
-  'alKategoriak',
-  () =>
-    getItems({
-      collection: 'termekAlKategoria',
-      fields: ['id', 'termekAlKategoriaNev', 'termekAlKategoriaKep', 'slug'],
-      params: {
-        filter: { kapcsolodoKategoria: { _eq: '1' } },
-      },
-    })
+const { data } = await useAsyncData('alKategoriak', () =>
+  getItems({
+    collection: 'termekAlKategoria',
+    fields: ['id', 'termekAlKategoriaNev', 'termekAlKategoriaKep', 'slug'],
+    params: {
+      filter: { kapcsolodoKategoria: { slug: props.categorySlug } },
+    },
+  })
 );
 
-// console.log(kerekpartartokAlKategoriak.value);
+const computedThumbnail = computed(() => {
+  if (props.categorySlug === 'kerekpartartok') {
+    return allBikeRacksThumbnail;
+  }
+
+  if (props.categorySlug === 'tetocsomagtartok') {
+    return allRoofRacksThumbnail;
+  }
+
+  if (props.categorySlug === 'csomagtarto-boxok-es-kosarak') {
+    return allRoofBoxesThumbnail;
+  }
+});
+
+const computedTitle = computed(() => {
+  if (props.categorySlug === 'kerekpartartok') {
+    return 'Összes kerékpártartó';
+  }
+
+  if (props.categorySlug === 'tetocsomagtartok') {
+    return 'Összes tetőcsomagtartó';
+  }
+
+  if (props.categorySlug === 'csomagtarto-boxok-es-kosarak') {
+    return 'Összes box és kosár';
+  }
+});
 </script>
 
 <template>
@@ -27,19 +56,19 @@ const { data: kerekpartartokAlKategoriak } = await useAsyncData(
         class="flex flex-row px-1 py-10 overflow-x-scroll overflow-y-hidden 2xl:site-padding 2xl:overflow-x-hidden gap-x-4"
       >
         <CategoryNavbarItem
-          title="Összes kerékpártartó"
-          :image="allBikeRacksThumbnail"
-          to="/kerekpartartok"
-          :is-active="path === `/kerekpartartok`"
+          :title="computedTitle"
+          :image="computedThumbnail"
+          :to="`/${props.categorySlug}`"
+          :is-active="path === `/${props.categorySlug}`"
         />
         <CategoryNavbarItem
-          v-for="thumb in kerekpartartokAlKategoriak"
+          v-for="thumb in data"
           :key="thumb.id"
           :id="thumb.id"
           :title="thumb.termekAlKategoriaNev"
           :image="thumb.termekAlKategoriaKep"
-          :to="`/kerekpartartok/${thumb.slug}`"
-          :is-active="path === `/kerekpartartok/${thumb.slug}`"
+          :to="`/${props.categorySlug}/${thumb.slug}`"
+          :is-active="path === `/${props.categorySlug}/${thumb.slug}`"
         />
       </div>
     </div>
