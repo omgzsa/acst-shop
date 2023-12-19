57527752d18467caef1d405f2385c58dd63a7124
omgzsa <zselloandras@gmail.com>
2023-12-19 16:28:21 +0100

    borondok-es-utazotaskak

---
+++ pages/borondok-es-utazotaskak/index.vue
@@ -0,0 +1,52 @@
+<script setup>
+const { path } = useRoute();
+const { getItems } = useDirectusItems();
+
+const { data: borondokEsUtazotaskak } = await useAsyncData(path, () =>
+  getItems({
+    collection: 'termekek',
+    params: {
+      fields: [
+        'id',
+        'termekNev',
+        'termekKep',
+        'termekLeiras',
+        'kapcsolodoKategoria.slug',
+        'kapcsolodoAlKategoria.slug',
+      ],
+      filter: {
+        kapcsolodoKategoria: { id: { _eq: '7' } },
+      },
+    },
+  })
+);
+
+const { pageQuantity, filteredItems } = usePageProperties(
+  borondokEsUtazotaskak.value
+);
+</script>
+
+<template>
+  <div class="bg-white">
+    <CategoryNavbar category-slug="borondok-es-utazotaskak" />
+    <div class="pt-14 space-y-14 site-padding">
+      <AppHeader>
+        <template #title> Bőröndök és utazótáskák </template>
+        <template #description>
+          Érezd az adrenalinszint emelkedését, amely egy új hely felfedezésével
+          jár. Az utazótáskáink, bőröndjeink és egyéb utazótáskák felejthetetlen
+          utazásokra készültek.
+        </template>
+      </AppHeader>
+
+      <!-- product filters section -->
+      <ProductFiltersBorondokEsUtazotaskak
+        :quantity="pageQuantity"
+        :is-disabled="true"
+      />
+
+      <!-- product archive -->
+      <ProductList :items="filteredItems" />
+    </div>
+  </div>
+</template>
