0a69b528125bdb9d660de706a3e94fbddde1441f
omgzsa <zselloandras@gmail.com>
2023-12-20 11:01:57 +0100

    kerekparos-es-sporttaskak pages

---
+++ pages/kerekparos-es-sporttaskak/[category]/[id]-[name].vue
@@ -0,0 +1,130 @@
+<script setup>
+import { useCartStore } from '@/stores/cart';
+const { getItemById } = useDirectusItems();
+const { params, path } = useRoute();
+
+const product = ref({});
+const cartStore = useCartStore();
+
+const { data, pending, error } = await useAsyncData(path, () => {
+  return getItemById({
+    collection: 'termekek',
+    id: params.id,
+    params: {
+      fields: [
+        'id',
+        'termekNev',
+        'termekKep',
+        'termekLeiras',
+        'termekReszletek',
+        'termekAr',
+        'eredetiThuleGarancia',
+        'termekGaleria.id',
+        'termekGaleria.directus_files_id',
+        'kerekparosEsSportTechSpec.*',
+      ],
+    },
+  });
+});
+
+const { data: techSpec } = await useAsyncData('techSpec', () => {
+  return getItemById({
+    collection: `kerekparosEsSportTechSpec`,
+    id: data.value.kerekparosEsSportTechSpec[0].id,
+    filter: {
+      kapcsolodoTermekek: { id: { _eq: params.id } },
+    },
+  });
+});
+const noTechSpec = computed(() => {
+  return techSpec.value === null;
+});
+
+if (!data.value) {
+  throw createError({
+    statusCode: 404,
+    message: 'Nem találjuk a terméket 💥💥',
+    fatal: true,
+  });
+}
+
+product.value = data.value;
+</script>
+
+<template>
+  <div class="bg-white">
+    <div class="site-padding">
+      <TheBreadCrumbs />
+      <!-- <div
+        v-if="pending"
+        class="flex items-center justify-center h-12 text-xl font-bold text-white bg-red-500"
+      >
+        LOADING ...
+      </div>
+      <div v-if="error" class="h-12 mx-auto my-auto bg-red-400">
+        {{ error.message }}
+      </div> -->
+    </div>
+    <div v-if="product">
+      <div class="grid lg:grid-cols-3 gap-x-6 gap-y-10 site-padding">
+        <ProductGallery :gallery="product.termekGaleria" />
+        <ProductInfo
+          :id="product.id"
+          :name="product.termekNev"
+          :img="product.termekGaleria[0].directus_files_id"
+          :description="product.termekLeiras"
+          :price="product.termekAr"
+          :details="product.termekReszletek"
+          :has-varranty="product.eredetiThuleGarancia"
+          @add-to-cart="cartStore.addItems($event)"
+        />
+        <ProductTechSpec v-if="!noTechSpec">
+          <ProductTechSpecItem
+            name="Méret (HxSZxM)"
+            type="string"
+            :value="techSpec.meret_HxSZxM"
+            v-if="techSpec.meret_HxSZxM !== null"
+          />
+          <ProductTechSpecItem
+            name="Űrtartalom"
+            type="number"
+            measure="L"
+            :value="techSpec.urtartalom"
+            v-if="techSpec.urtartalom !== null"
+          />
+          <ProductTechSpecItem
+            name="Súly"
+            type="number"
+            measure="kg"
+            :value="techSpec.suly"
+            v-if="techSpec.suly !== null"
+          />
+          <ProductTechSpecItem
+            name="Szín"
+            type="string"
+            :value="techSpec.szin"
+            v-if="techSpec.szin !== null"
+          />
+          <ProductTechSpecItem
+            name="Vízállóság"
+            type="string"
+            :value="techSpec.vizallosag"
+            v-if="techSpec.vizallosag !== null"
+          />
+          <ProductTechSpecItem
+            name="Anyaga"
+            type="string"
+            :value="techSpec.anyaga"
+            v-if="techSpec.anyaga !== null"
+          />
+          <ProductTechSpecItem
+            name="Típusszám"
+            type="number"
+            :value="techSpec.tipusszam"
+            v-if="techSpec.tipusszam !== null"
+          />
+        </ProductTechSpec>
+      </div>
+    </div>
+  </div>
+</template>
