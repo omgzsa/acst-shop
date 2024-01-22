cba43c3122dec14b35ff83ed945a1187eb424d90
omgzsa <zselloandras@gmail.com>
2024-01-22 16:59:23 +0100

    style update

--- components/home/featured.vue
+++ components/home/featured.vue
@@ -42,7 +42,7 @@
           sosem volt még ilyen egyszerű mint a THULE csomagtartóival.
         </p>
         <button
-          class="px-4 py-2 space-x-2 font-semibold border rounded-md shadow-md border-accent-100 duration-400 bg-accent-100 text-dark-100 hover:bg-dark-100 hover:text-white hover:shadow-lg"
+          class="px-4 py-2 space-x-2 font-semibold border shadow-md border-accent-100 duration-400 bg-accent-100 text-dark-100 hover:bg-dark-100 hover:text-white hover:shadow-lg"
         >
           <NuxtLink to="/">
             <span class="text-base leading-snug tracking-wide xl:text-lg">
