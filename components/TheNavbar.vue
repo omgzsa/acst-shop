e5aa072758522a84920de7b3e0bb4e567011e658
omgzsa <zselloandras@gmail.com>
2024-02-15 16:23:23 +0100

    lowered z-index

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -20,7 +20,7 @@ watch(
 </script>
 
 <template>
-  <header class="fixed top-0 left-0 z-50 w-full bg-white shadow text-dark-100">
+  <header class="fixed top-0 left-0 z-20 w-full bg-white shadow text-dark-100">
     <nav class="site-padding h-14">
       <div class="relative flex items-center justify-between h-full space-x-6">
         <!-- LOGO -->
