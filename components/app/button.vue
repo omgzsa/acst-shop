5a333c26b09f6067a6f8befac9b1b755987fb286
omgzsa <zselloandras@gmail.com>
2024-02-16 19:55:09 +0100

    feat: added disabled prop w/ styles

--- components/app/button.vue
+++ components/app/button.vue
@@ -1,17 +1,19 @@
 <script setup>
-const { defaultTransition } = useTailwindConfig();
-
 defineProps({
   label: String,
   to: String,
+  disabled: Boolean,
 });
 </script>
 
 <template>
   <NuxtLink :to="to">
     <button
-      class="px-4 py-2 space-x-2 font-semibold border rounded-md shadow-md border-accent-100 duration-400 bg-accent-100 text-dark-100 hover:bg-dark-100 hover:text-white hover:shadow-lg"
-      :class="defaultTransition"
+      class="px-4 py-2 space-x-2 font-semibold transition-all ease-in-out border rounded-md shadow-md border-accent-100 duration-250 bg-accent-100 text-dark-100 hover:bg-dark-100 hover:text-white hover:shadow-lg"
+      :class="{
+        'cursor-not-allowed': disabled,
+      }"
+      :disabled="disabled"
     >
       <span class="text-base leading-snug tracking-wide xl:text-lg">
         {{ label }}
