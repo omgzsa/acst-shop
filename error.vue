4481b2e66d98cdb3ac2cf37ce3564f8a8317c7d7
omgzsa <zselloandras@gmail.com>
2024-02-25 19:39:32 +0100

    style: update on error page

--- error.vue
+++ error.vue
@@ -9,14 +9,16 @@ const handleClearError = () => clearError({ redirect: '/' });
 </script>
 
 <template>
-  <div class="grid items-center h-[85vh] site-padding">
-    <div class="max-w-md p-16 m-auto space-y-8 border border-dark-100">
-      <p class="text-6xl font-bold">{{ error.statusCode }}</p>
-      <p class="text-2xl">Oooops... az oldal amit keresel nem létezik.</p>
-      <p class="">{{ error.message }}</p>
+  <div class="absolute top-0 z-50 grid items-center w-full h-full bg-dark-100">
+    <div class="max-w-md p-16 m-auto space-y-8">
+      <p class="text-6xl font-bold text-neutral-200">{{ error.statusCode }}</p>
+      <p class="text-2xl text-neutral-300">
+        Oooops... az oldal amit keresel nem létezik.
+      </p>
+      <p class="text-neutral-300">{{ error.message }}</p>
       <button
         :class="defaultTransition"
-        class="px-4 py-2 border mt-7 border-dark-100 hover:bg-dark-100 hover:text-white"
+        class="px-4 py-2 border mt-7 border-dark-300 hover:bg-accent-100 hover:text-dark-100 text-neutral-300"
         @click="handleClearError"
       >
         Vissza
