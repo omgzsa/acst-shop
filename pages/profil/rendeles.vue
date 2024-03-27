5bfb681bd0d622db657fb1875a59a29ae206804d
omgzsa <zselloandras@gmail.com>
2024-03-27 16:10:13 +0100

    feat: fleshed out orders table with correct data, needs work on preview modal

--- pages/profil/rendeles.vue
+++ pages/profil/rendeles.vue
@@ -4,82 +4,118 @@ definePageMeta({
 });
 
 const user = useDirectusUser();
+const orderInModal = ref(null);
+const show = ref(false);
 
 // const { orders } = useGetOrders();
 
 const { getItems } = useDirectusItems();
-
 const { data } = await useAsyncData(() => {
   return getItems({
     collection: 'vasarlasok',
     params: {
-      fields: ['PaymentId', 'PaymentStatus', 'teljesOsszeg', 'vasarlo'],
+      fields: [
+        'date_created',
+        'PaymentId',
+        'PaymentStatus',
+        'teljesOsszeg',
+        'vasarlo',
+      ],
       filter: {
         vasarlo: user.value.id,
+        // PaymentStatus: { _eq: 'Succeeded' },
+        PaymentStatus: { _in: ['Succeeded', 'Expired', 'Canceled'] },
       },
       sort: ['-date_created'],
     },
   });
 });
 
-console.log(data.value);
+function formatDate(date) {
+  return new Date(date).toLocaleDateString('hu-HU');
+}
+
+const handleOpen = (e, item) => {
+  show.value = true;
+  orderInModal.value = item;
+};
 </script>
 
 <template>
   <div class="bg-white">
     <div class="py-10 site-padding">
+      <h1 class="pb-8">Rendeléseim</h1>
       <div class="grid grid-cols-6">
-        <div class="col-span-1">
+        <!-- 
+          PROFILE NAVIGATION
+         -->
+        <div class="pb-8 md:m-0 col-span-full md:col-span-1">
           <ProfileNav />
         </div>
-        <div v-if="data" class="flex flex-col col-span-5 space-y-4">
-          <table class="min-w-full divide-y divide-gray-300">
-            <thead class="bg-gray-50">
+        <div v-if="data" class="overflow-x-auto col-span-full md:col-span-5">
+          <table class="min-w-full text-sm bg-white divide-y-2 divide-gray-200">
+            <!-- 
+              TABLE HEADER
+             -->
+            <thead class="text-left">
               <tr>
                 <th
-                  scope="col"
-                  class="px-3 py-3 text-xs font-medium tracking-wide text-left uppercase text-dark-100"
+                  class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap"
                 >
                   Payment ID
                 </th>
                 <th
-                  scope="col"
-                  class="px-3 py-3 text-xs font-medium tracking-wide text-left uppercase text-dark-100"
+                  class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap"
                 >
                   Status
                 </th>
                 <th
-                  scope="col"
-                  class="px-3 py-3 text-xs font-medium tracking-wide text-left uppercase text-dark-100"
+                  class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap"
+                >
+                  Date
+                </th>
+                <th
+                  class="px-4 py-2 font-medium text-gray-900 whitespace-nowrap"
                 >
                   Price
                 </th>
-                <th scope="col" class="relative py-3 pl-3 pr-4 sm:pr-6">
-                  <span class="sr-only">Edit</span>
-                </th>
+                <th class="px-4 py-2"></th>
               </tr>
             </thead>
-            <tbody class="bg-white divide-y divide-gray-200">
+
+            <!-- 
+              TABLE BODY
+             -->
+            <tbody class="divide-y divide-gray-200">
               <tr v-for="i in data" :key="i.PaymentId">
-                <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
+                <td class="px-4 py-2 text-gray-700 whitespace-nowrap">
                   {{ i.PaymentId }}
                 </td>
-                <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
+                <td class="px-4 py-2 text-gray-700 whitespace-nowrap">
                   {{ i.PaymentStatus }}
                 </td>
-                <td class="px-3 py-4 text-sm text-gray-500 whitespace-nowrap">
-                  {{ i.teljesOsszeg }} Ft
+                <td class="px-4 py-2 text-gray-700 whitespace-nowrap">
+                  {{ formatDate(i.date_created) }}
                 </td>
-                <td
-                  class="relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6"
-                >
-                  <a href="#" class="text-indigo-600 hover:text-indigo-900"
-                    >Details</a
+                <td class="px-4 py-2 text-gray-700 whitespace-nowrap">
+                  {{ i.teljesOsszeg.toLocaleString('hu-HU') }} Ft
+                </td>
+                <td class="px-4 py-2 whitespace-nowrap">
+                  <button
+                    @click="handleOpen($event, i)"
+                    class="inline-block px-3 py-1 space-x-2 font-semibold transition-all shadow ring-1 ring-accent-100 duration-400 bg-accent-100 text-dark-100 hover:bg-dark-100 hover:text-white hover:shadow-lg"
                   >
+                    Megnézem
+                  </button>
                 </td>
               </tr>
             </tbody>
           </table>
+          <AppModal
+            v-model="show"
+            :order="orderInModal"
+            @confirm="() => (show = false)"
+          />
         </div>
       </div>
     </div>
