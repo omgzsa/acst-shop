e6f47742d23f961a7c6263df4df3dd3803c7f7f1
omgzsa <zselloandras@gmail.com>
2024-03-27 16:10:34 +0100

    footer contact wrapper/item

---
+++ components/footer/contact/index.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+defineProps({
+  address: String,
+  phone: String,
+  email: String,
+  openingHours: Object,
+});
+</script>
+
+<template>
+  <div class="flex flex-col col-span-1 space-y-2">
+    <FooterContactItem icon="mdi:map-marker">
+      <a class="footer-link hover:text-accent-100 text-zinc-50">
+        {{ address }}
+      </a>
+    </FooterContactItem>
+    <FooterContactItem icon="mdi:clock-time-eight">
+      <ul>
+        <li v-for="day in openingHours" :key="day" class="text-zinc-50">
+          {{ day }}
+        </li>
+      </ul>
+    </FooterContactItem>
+    <FooterContactItem icon="mdi:phone">
+      <a class="footer-link hover:text-accent-100 text-zinc-50">
+        {{ phone }}
+      </a>
+    </FooterContactItem>
+    <FooterContactItem icon="mdi:email">
+      <a class="footer-link hover:text-accent-100 text-zinc-50">
+        {{ email }}
+      </a>
+    </FooterContactItem>
+  </div>
+</template>
--- components/footer/contact/item.vue
+++ components/footer/contact/item.vue
@@ -6,7 +6,7 @@ defineProps({
 
 <template>
   <div class="flex items-start gap-x-2">
-    <Icon :name="icon" class="w-5 h-5" />
+    <Icon :name="icon" class="w-5 h-5 text-accent-100" />
     <slot />
   </div>
 </template>
