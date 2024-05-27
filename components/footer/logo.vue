6c237913b8c724894ec0718c42ed74f21f6e2b34
omgzsa <zselloandras@gmail.com>
2024-05-27 20:33:22 +0200

    update spacings

--- components/footer/logo.vue
+++ components/footer/logo.vue
@@ -3,10 +3,10 @@
 </script>
 
 <template>
-  <div class="flex flex-col items-center pt-10 col-span-full">
+  <div class="flex flex-col pt-10 col-span-full">
     <NuxtImg
       src="/thule-logo-black-footer.png"
-      class="object-contain object-left h-20 w-60"
+      class="object-contain object-left h-full w-60"
     />
     <p class="pb-2 font-semibold text-white uppercase">
       Hivatalos márka partner
